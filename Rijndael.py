bcc31c36e65aced2113a9f7501954bb88909733f
SymmetricChaos <ajfraebel@gmail.com>
2019-03-16 08:22:26 -0400

    Create Rijndael.py
    The Rijndael field

---
+++ Rijndael.py
@@ -0,0 +1,17 @@
+from PolynomialDivision import poly_add, poly_divmod, poly_print
+
+# The finite field GF(2^8) with 256 elements has various expressions
+
+
+def Rijndael_add(P,Q):
+    
+    # The Rijndael polynomial 
+    R = [1,1,0,1,1,0,0,0,1]
+    
+    S = poly_add(P,Q,2)
+    S = poly_divmod(S,R,2)[1]
+    
+    poly_print(S)
+    
+
+Rijndael_add([0,1,1,0,1,1,1],[1,0,1,0,1])
