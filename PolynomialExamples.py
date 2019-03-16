a401f5b5483300d76819994fd3f5475f086ec0ed
SymmetricChaos <ajfraebel@gmail.com>
2019-03-16 08:22:04 -0400

    Create PolynomialExamples.py
    Examples in own file

---
+++ PolynomialExamples.py
@@ -0,0 +1,19 @@
+from PolynomialDivision import *
+
+P = [-42,1,-12,-1,0,0]
+Q = [-3,1,0,0]
+
+print(P)
+poly_print(P)
+print()
+
+P = [0,1,1,1,1,1,1,0,1,1,1,1,1,1]
+Q = [1,1,0,1,1,0,0,0,1]
+
+#print(polyadd(P,Q,2))
+#print(polymult([1,0,2,1,0],[1,1,2,1],3))
+
+print(P)
+print(Q)
+print()
+print(poly_divmod(P,Q,2))
