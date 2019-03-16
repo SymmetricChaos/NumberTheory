5374649a009000bfa5ef474cfab348aa0d25f103
SymmetricChaos <ajfraebel@gmail.com>
2019-03-16 08:40:00 -0400

    Update PolynomialExamples.py

--- PolynomialExamples.py
+++ PolynomialExamples.py
@@ -1,19 +1,17 @@
-from PolynomialRepresentation import *
+from PolynomialRepresentation import poly_norm, poly_degree, poly_pad, \
+                                     poly_print, poly_add, poly_mult, \
+                                     poly_divmod
 
-P = [1,-42,1,-12,-1,0,0]
+# Four polynomials
+P = [-42,1,-12,-1]
 Q = [-3,1,0,0]
+R = [9,6,7,1,3,4,5]
+S = [1,1,0,1,1,0,0,0,1]
 
-print(P)
-poly_print(P)
-print()
+print("Four Polynomials in Ascending Vector Form")
+for i in [P,Q,R,S]:
+    print(i)
 
-P = [0,1,1,1,1,1,1,0,1,1,1,1,1,1]
-Q = [1,1,0,1,1,0,0,0,1]
-
-#print(polyadd(P,Q,2))
-#print(polymult([1,0,2,1,0],[1,1,2,1],3))
-
-print(P)
-print(Q)
-print()
-print(poly_divmod(P,Q,2))
+print("\nThe Same Polynomials in Descemding Written Form")
+for i in [P,Q,R,S]:
+    poly_print(i)
