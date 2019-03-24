92f03569f9736235ae87406d87170e0c92f38f05
SymmetricChaos <ajfraebel@gmail.com>
2019-03-24 13:21:15 -0400

    Update PrimitiveRoot.py
    Better comments

--- PrimitiveRoot.py
+++ PrimitiveRoot.py
@@ -1,11 +1,11 @@
 from ModularArithmetic import coprimes
 
 # A pimitive root modulo n g^k = a mod n for every a coprime to n
-# Equivalently a primitive root is a generating elements for some subgroups of a multiplicative group
+# Equivalently a primitive root is a generating element of the multiplicative 
+# group modulo n
 
 # Show all the congruences
 def show_congruences(g,m):
-    print("modulo {}".format(m))
     # choose the width 
     W = len(str(m))
     for i in range(m):
@@ -13,9 +13,14 @@ def show_congruences(g,m):
     print()
     
 def primitive_roots(m):
+    # List the numbers coprime to m since primitive roots must be coprime and
+    # they must generate the set of all the numbers coprime to m
     C = coprimes(m)
     L = []
     out = []
+    
+    # Try each coprime number and see if it generates the set C
+    # If it does write down that number otherwise try again
     for g in C:
         for i in range(m):
             L.append(g**i % m)
@@ -26,7 +31,7 @@ def primitive_roots(m):
         
     return out
 
-m = 10
+m = 18
 r = primitive_roots(m)
 print("numbers coprime to {}\n{}\n".format(m,coprimes(m)))
 print("primitive roots mod {}\n{}\n".format(m,r))
