0e9423bb649b42e3257d46575531c0ebfb7eb373
SymmetricChaos <ajfraebel@gmail.com>
2019-03-24 18:21:37 -0400

    Lucas Numbers

--- Sequences/Fibonacci.py
+++ Sequences/Fibonacci.py
@@ -14,3 +14,15 @@ def fibonacci(n=0):
         for i in range(n):
             a, b = b, a+b
             yield a
+
+# Fibonacci numbers starting from zero
+def fibonacci_zero(n=0):
+    yield 0
+    if n == 0:
+        for i in fibonacci():
+            yield i
+    if n == 1:
+        yield 1
+    else:
+        for i in fibonacci(n-1):
+            yield i
