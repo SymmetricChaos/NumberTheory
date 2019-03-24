7dd0aec896bb811e984ff5f32b6efe9421b89b81
SymmetricChaos <ajfraebel@gmail.com>
2019-03-24 16:29:33 -0400

    Create Primes.py

---
+++ Sequences/Primes.py
@@ -0,0 +1,14 @@
+## Generator that returns primes (not my work)
+def primes():
+    D = {}
+    q = 2
+    while True:
+        if q not in D:
+
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        q += 1
