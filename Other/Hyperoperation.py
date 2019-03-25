bf2e2e47ac6ead475570dabe8c3344514d3dbcc8
SymmetricChaos <ajfraebel@gmail.com>
2019-03-25 07:49:02 -0400

    Create Hyperoperation.py

---
+++ Other/Hyperoperation.py
@@ -0,0 +1,28 @@
+import sys
+sys.setrecursionlimit(5000)
+#def hyper(a,b,n):
+#    if n == 0:
+#        return b+1
+#    if n == 1 and b == 0:
+#        return a
+#    if n == 2 and b == 0:
+#        return 0
+#    if n >= 3 and b == 0:
+#        return 1
+#    else:
+#        return hyper(a,hyper(a,b-1,n),n-1)
+
+def hyper(a,b,n):
+    #print(b)
+    if n == 0:
+        return b+1
+    if n == 1 and b == 0:
+        return a
+    if n == 2 and b == 0:
+        return 0
+    if n >= 3 and b == 0:
+        return 1
+    else:
+        return hyper(a,hyper(a,b-1,n),n-1)
+
+print(hyper(5,2,4))
