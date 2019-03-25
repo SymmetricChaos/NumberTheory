0962aa51ffc3272e35dccf27ec82a584ae8026ed
SymmetricChaos <ajfraebel@gmail.com>
2019-03-25 17:54:29 -0400

    Update __init__.py
    Hyperoperation Added

--- Other/__init__.py
+++ Other/__init__.py
@@ -1,4 +1,4 @@
 from Other.Factors import factors, prime_factorization
 from Other.LongDivision import long_division
-
-__all__=["factors","long_division","prime_factorization"]
+from Other.Hyperoperation import hyper
+__all__=["factors","long_division","prime_factorization","hyper"]
