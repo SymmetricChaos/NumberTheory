200f3cd89482678d971df2ae2202e8090a97bff0
SymmetricChaos <ajfraebel@gmail.com>
2019-03-28 19:16:26 -0400

    Create Wrappers.py
    Uses sequence_maker to produce more convenient generators

---
+++ Sequences/Wrappers.py
@@ -0,0 +1,8 @@
+from Sequences.Utils import sequence_maker
+from Sequences.Naturals import naturals_inf
+from Sequences.Recurrence import fibonacci_inf, lucas_inf, pell_inf
+
+naturals = sequence_maker(naturals_inf)
+fibonacci = sequence_maker(fibonacci_inf)
+lucas = sequence_maker(lucas_inf)
+pell = sequence_maker(pell_inf)
