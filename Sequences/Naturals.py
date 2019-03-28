ac2c0429a25ea5a6814ae851957d3bc7ecdf43b6
SymmetricChaos <ajfraebel@gmail.com>
2019-03-28 19:17:40 -0400

    Update Naturals.py
    Simplified naturals for new system

--- Sequences/Naturals.py
+++ Sequences/Naturals.py
@@ -1,47 +1,20 @@
-def naturals(n=0,offset=0,m=0):
-    
-    if offset > n:
-        raise Exception("Offset cannot be greater than sequence length")
-    
-    if n == 0:
-        n = float('Inf')
-    if m == 0:
-        m = float('Inf')
-    
+def naturals_inf():
+
     ctr = 0
     
     while True:
-        if ctr >= offset:
-            yield ctr
+        yield ctr
         
         ctr += 1
-        
-        if ctr > m:
-            break
-        
-        if ctr == n+offset:
-            break
 
-def integers(n=0,offset=0,m=0):
-    
-    if offset > n:
-        raise Exception("Offset cannot be greater than sequence length")
-    
-    if n == 0:
-        n = float('Inf')
-    if m == 0:
-        m = float('Inf')
+def integers_inf():
     
     ctr = 0
     v = 0
     
     while True:
         
-        if v > m:
-            break
-        
-        if ctr >= offset:
-            yield v
+        yield v
         
         ctr += 1
         if ctr % 2 == 0:
@@ -49,10 +22,3 @@ def integers(n=0,offset=0,m=0):
         else:
             v += ctr
         
-        if ctr == n+offset:
-            break
-
-for i in naturals(20):
-    print(i)
-for i in integers(20):
-    print(i)
