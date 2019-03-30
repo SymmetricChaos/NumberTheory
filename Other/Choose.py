4a1f56444756d689341847e1c32a28f57803dc4e
SymmetricChaos <ajfraebel@gmail.com>
2019-03-30 19:29:18 -0400

    Create Choose.py

---
+++ Other/Choose.py
@@ -0,0 +1,15 @@
+def choose(n,k):
+    """Binomial coefficient"""
+    if n < k:
+        raise Exception("n must be greater than k")
+    if k < 0:
+        raise Exception("k must be nonnegative")
+        
+    # Calculate the numerator and denominator seperately in order to avoid loss
+    # of precision for large numbers.
+    N = 1
+    D = 1
+    for i in range(1,k+1):
+        N *= (n+1-i)
+        D *= i
+    return N//D
