50476830de3b47af9cfb13c51ab7f62286b2544a
SymmetricChaos <ajfraebel@gmail.com>
2019-03-30 19:31:02 -0400

    Update __init__.py
    More stuff

--- Sequences/__init__.py
+++ Sequences/__init__.py
@@ -1,13 +1,14 @@
-#from Sequences.Primes import primes
 from Sequences.Recurrence import fibonacci, lucas, pell, pell_lucas, tribonacci, \
                                  P_fibonacci, PQ_fibonacci, padovan
                                  
-from Sequences.Polygonal import triangular, pentagonal, gen_pentagonal, polygonal, gen_polygonal
+from Sequences.Polygonal import triangular, pentagonal, gen_pentagonal, polygonal, \
+                                gen_polygonal, simplicial, cen_polygonal
 
-from Sequences.Naturals import  naturals, integers
+from Sequences.Naturals import naturals, integers
                                  
+from Sequences.Primes import primes, primorials
 
-
-__all__=["lucas","fibonacci","naturals","pell","pell_lucas",
+__all__=["lucas","fibonacci","naturals","pell","pell_lucas","primes","primorials",
          "tribonacci","P_fibonacci", "PQ_fibonacci","padovan","integers",
-         "triangular","pentagonal","gen_pentagonal","gen_polygonal"]
+         "triangular","pentagonal","gen_pentagonal","polygonal","gen_polygonal",
+         "simplicial","cen_polygonal"]
