cf7556363819131714ec96139d03102774aed5a4
SymmetricChaos <ajfraebel@gmail.com>
2019-03-30 20:15:44 -0400

    organized

--- Sequences/__init__.py
+++ Sequences/__init__.py
@@ -1,8 +1,11 @@
 from Sequences.Recurrence import fibonacci, lucas, pell, pell_lucas, tribonacci, \
                                  P_fibonacci, PQ_fibonacci, padovan
                                  
-from Sequences.Polygonal import triangular, pentagonal, gen_pentagonal, polygonal, \
-                                gen_polygonal, simplicial, cen_polygonal
+from Sequences.Polygonal import triangular, gen_triangular, cen_triangular, \
+                                square, gen_square, cen_square, \
+                                pentagonal, cen_pentagonal, gen_pentagonal, \
+                                polygonal, gen_polygonal, cen_polygonal, \
+                                simplicial
 
 from Sequences.Naturals import naturals, integers
                                  
@@ -10,5 +13,8 @@ from Sequences.Primes import primes, primorials
 
 __all__=["lucas","fibonacci","naturals","pell","pell_lucas","primes","primorials",
          "tribonacci","P_fibonacci", "PQ_fibonacci","padovan","integers",
-         "triangular","pentagonal","gen_pentagonal","polygonal","gen_polygonal",
-         "simplicial","cen_polygonal"]
+         "triangular", "gen_triangular", "cen_triangular",
+         "square", "gen_square", "cen_square",
+         "pentagonal","cen_pentagonal","gen_pentagonal",
+         "polygonal","cen_polygonal","gen_polygonal",
+         "simplicial"]
--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,21 +1,21 @@
 from Sequences import naturals, fibonacci, lucas, pell, pell_lucas, tribonacci, \
-                               P_fibonacci, PQ_fibonacci, padovan, integers, triangular, \
-                               pentagonal, gen_pentagonal, gen_polygonal, polygonal,\
-                               primes, primorials, simplicial, cen_polygonal
+                               P_fibonacci, PQ_fibonacci, padovan, integers, \
+                               triangular, gen_triangular, cen_triangular, \
+                               square, gen_square, cen_square, \
+                               pentagonal, cen_pentagonal, gen_pentagonal, \
+                               primes, primorials, simplicial
 
 from Sequences.Utils import show_vals
 
 
-
-
-
-for seq in [naturals, integers, fibonacci, lucas, pell, pell_lucas, tribonacci,
-            padovan, triangular, pentagonal, gen_pentagonal, primes, primorials]:
+for seq in [naturals, integers, 
+            triangular, gen_triangular, cen_triangular,
+            square, gen_square, cen_square,
+            pentagonal, cen_pentagonal, gen_pentagonal,
+            fibonacci, lucas, pell, pell_lucas, tribonacci, padovan, 
+            primes, primorials]:
     show_vals(seq)
 
 show_vals(simplicial,D=3)
 show_vals(P_fibonacci,P=3)
 show_vals(PQ_fibonacci,P=3,Q=2)
-show_vals(polygonal,S=7)
-show_vals(cen_polygonal,S=7)
-show_vals(gen_polygonal,S=7)
