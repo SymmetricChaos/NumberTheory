400dbc0de96282d79ab478bf79b640b6fdb0ece1
SymmetricChaos <ajfraebel@gmail.com>
2019-04-01 20:13:30 -0400

    Update Utils.py
    Added docstrings.

--- ModularArithmetic/Utils.py
+++ ModularArithmetic/Utils.py
@@ -1,8 +1,9 @@
 # Extended Euclidean algorithm
-# Very useful for a bunch of functions in modular arithmetics
+# Very useful for a bunch of functions in modular arithmetic
 # g   : Greatest common denominator
 # x,y : integers such that g = ax + by
 def egcd(a, b):
+    """Extended Euclidean Algorithm"""
     if a == 0:
         return (b, 0, 1)
     else:
@@ -11,6 +12,7 @@ def egcd(a, b):
 
 # Determine the greatest common denominator for a set of numbers
 def gcd(*args):
+    """Greatest Common Denominator"""
     # simplest case
     if len(args) == 1:
         return args[0]
@@ -30,7 +32,7 @@ def gcd(*args):
 
 # Determine the least common multiple for a set of numbers
 def lcm(*args):
-    
+    """Least Common Multiple"""
     # simplest case
     if len(args) == 1:
         return args[0]
@@ -50,6 +52,7 @@ def lcm(*args):
 
 # Use egcd to calculate the modular multiplicative inverse
 def modinv(a, m):
+    """Modular Multiplicative Inverse"""
     g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
@@ -58,13 +61,33 @@ def modinv(a, m):
 
 # Calculate the numbers coprime to some modulus m
 def coprimes(m):
+    """Numbers Coprime to Input"""
     L = []
     for i in range(1,m):
         if gcd(i,m) == 1:
             L.append(i)
     return L
 
+# Check if two numbers are coprimes
 def coprime(p,q):
+    """Check if inputs are coprime"""
     if gcd(p,q) == 1:
         return True
     return False
+
+
+from Sequences import primes
+def totient(n):
+    N = 1
+    D = 1
+    for p in primes():
+        if p > n:
+            break
+        
+        if n % p == 0:
+            N *= (p-1)
+            D *= p
+    
+    return n*N//D
+        
+        
