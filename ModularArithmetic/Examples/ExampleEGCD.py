3a82532163f61d5d649faa3c62e310ecf9ee5f0f
SymmetricChaos <ajfraebel@gmail.com>
2019-04-04 19:40:52 -0400

    Update ExampleEGCD.py
    Space to make sure Modular Arithmetic functions work

--- ModularArithmetic/Examples/ExampleEGCD.py
+++ ModularArithmetic/Examples/ExampleEGCD.py
@@ -1,3 +1,9 @@
-from ModularArithmetic.Utils import gcd
+from ModularArithmetic.Utils import gcd, lcm
 
-print(gcd([5,6]))
+print(gcd([60,6,12]))
+print(gcd(60,6,12))
+
+
+print(lcm([5,6,3]))
+
+print(lcm(5,6,3))
