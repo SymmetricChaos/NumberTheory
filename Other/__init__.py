f2641509a95f7d55225b5fdb159e1a4f4ea8a92b
SymmetricChaos <ajfraebel@gmail.com>
2019-04-06 09:38:37 -0400

    Folder for Computations of Things

--- Other/__init__.py
+++ Other/__init__.py
@@ -1,4 +1,3 @@
 from Other.Factorization import factorization, prime_factorization
-from Other.LongDivision import long_division
 from Other.Hyperoperation import hyper
-__all__=["factorization","long_division","prime_factorization","hyper"]
+__all__=["factorization","prime_factorization","hyper"]
