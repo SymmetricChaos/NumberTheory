49e518b155ee8667aba2bafb45155825069d091e
SymmetricChaos <ajfraebel@gmail.com>
2019-04-06 09:52:59 -0400

    Update PowerSeries.py
    Defaults to MacLaurin series

--- PowerSeries/PowerSeries.py
+++ PowerSeries/PowerSeries.py
@@ -1,11 +1,11 @@
-def power_series(x,A,c):
+def power_series(x,A,c=0):
     
     out = 0
     for n,a in enumerate(A):
         out += a*(x-c)**n
     return out
 
-def power_series_convergents(x,A,c):
+def power_series_convergents(x,A,c=0):
     
     out = 0
     for n,a in enumerate(A):
