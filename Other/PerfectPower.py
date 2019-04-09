d8d6a0a6c77887f7ce8ffaf210940839ce71e5d5
SymmetricChaos <ajfraebel@gmail.com>
2019-04-09 19:04:40 -0400

    Prime Powers Added

--- Other/PerfectPower.py
+++ Other/PerfectPower.py
@@ -1,4 +1,5 @@
-from math import log2, floor
+from math import log2, sqrt, floor
+from PrimeNumbers import primes
 
 def perfect_power(n):
     
@@ -7,7 +8,7 @@ def perfect_power(n):
     
     lim = floor(log2(n))+2
     
-    for i in range(2,lim+1):
+    for i in primes():
         ctr = 2
         while True:
             pwr = ctr**i
@@ -16,5 +17,32 @@ def perfect_power(n):
             if pwr == n:
                 return True
             ctr += 1
+        if i > lim:
+            break
 
     return False
+
+def prime_power(n):
+    
+    lim = floor(sqrt(n))+1
+    
+    div = False
+    for p in primes():
+        
+        if n % p == 0:
+            div = True
+            
+        while n % p == 0:
+            n = n // p
+        
+        
+        if div == True and n == 1:
+            return True
+        
+        if div == True and n != 1:
+            return False
+        
+        if p > lim:
+            break
+        
+    return True
