2ed566c8baf45fd5e260c407a41ff836e883bcba
SymmetricChaos <ajfraebel@gmail.com>
2019-04-09 19:04:02 -0400

    Primes in Prime Numbers

---
+++ PrimeNumbers/Primes.py
@@ -0,0 +1,17 @@
+## Generator that returns primes (not my work)
+def primes():
+    """Prime Numbers"""
+    D = {}
+    q = 2
+    
+    while True:
+        if q not in D:
+            
+            yield q
+                
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        q += 1
