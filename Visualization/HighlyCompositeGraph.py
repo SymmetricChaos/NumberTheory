d65338206416a730095ba5e9219523b9e5773c2c
SymmetricChaos <ajfraebel@gmail.com>
2019-04-11 18:34:25 -0400

    Visualization

--- Visualization/HighlyCompositeGraph.py
+++ Visualization/HighlyCompositeGraph.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 N = []
 L = []
 Hn = [2]
-Hl = [1]
-for i in range(2,200):
+Hl = [2]
+for i in range(2,2000):
     f = len(factorization(i))
     N.append(i)
     L.append(f)
@@ -13,6 +13,10 @@ for i in range(2,200):
     if f > Hl[-1]:
         Hn.append(i)
         Hl.append(f)
-    
-plt.bar(N,L)
-plt.scatter(Hn,Hl)
+
+print(Hn)
+
+fig = plt.figure()
+fig.set_size_inches(14, 6)
+plt.bar(N,L,6)
+plt.scatter(Hn,Hl,zorder=2)
