c26bce91c940a8f42bacca47c17bd2f00b34f551
SymmetricChaos <ajfraebel@gmail.com>
2019-04-10 20:30:50 -0400

    Inverses
    The inverse works at least

--- Other/EllipticCurveOOP.py
+++ Other/EllipticCurveOOP.py
@@ -31,6 +31,10 @@ class elliptic:
 def elliptic_add(P,Q,curve):
     if P not in curve.points or Q not in curve.points:
         raise Exception('Points must be elements of the curve')
+    
+    # If the points are inverses they add up to zero, which here is the point at infinity
+    if P == elliptic_inv(Q,curve):
+        return (float('inf'),float('inf'))
 
     if P == Q:
         tp = (3*P[0]*P[0]+curve.a) * (2*P[1])
@@ -40,14 +44,22 @@ def elliptic_add(P,Q,curve):
         m = modinv(tp, curve.field )
     
     x = (m*m - P[0] - Q[0]) % curve.field
-    y = (P[1] + m*(x - P[0])) % curve.field
+    y = -(P[1] + m*(x - P[0])) % curve.field
     
     if (x,y) not in curve.points:
         raise Exception('Something went wrong. The point {} is not on the curve.'.format((x,y)))
     
     return (x,y)
 
+def elliptic_inv(P,curve):
+    return (P[0],curve.field-P[1])
+
 curve = elliptic(2,3,97)
+print(curve.points)
+P = (3,6)
+Q = (3,6)
 
+print(P)
+print(Q)
 
-print(elliptic_add((3,6),(3,6),curve))
+print(elliptic_add(P,Q,curve))
