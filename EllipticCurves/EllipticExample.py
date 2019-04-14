f22aca5aa0e614c0c76a0c03f13a607ea2306ea4
SymmetricChaos <ajfraebel@gmail.com>
2019-04-14 09:39:23 -0400

    Much Better Examples

--- EllipticCurves/EllipticExample.py
+++ EllipticCurves/EllipticExample.py
@@ -1,9 +1,20 @@
 from EllipticCurves.EllipticPoint import Elliptic_Curve, Elliptic_Point, cyclic_subgroup, cyclic_subgroups
 
-C = Elliptic_Curve(2,3,223)
-P = C.points()
-a = Elliptic_Point(P[12][0],P[12][1],C)
 
-print([i.coords for i in cyclic_subgroup(a)])
-print()
+a, b, f = 2, 3, 29
+C = Elliptic_Curve(a,b,f)
+P = C.points()
+p = Elliptic_Point(P[8][0],P[8][1],C)
+
+
+print("The Elliptic Curve y^2 = x^3 + {}x + {} over GF({}) has {} points on it.".format(a,b,f,len(P)))
+print("Those points are:")
+print(P)
+
+print("\nEach of these points generates a cyclic subgroup of some order {}/n where n is a natural number.".format(len(P)))
+
+print("\nFor example repeated addition by {} gives the subgroup".format(P[8]))
+print([i.coords for i in cyclic_subgroup(p)])
+
+print("\nThe order of each subgroup is:")
 print([len(g) for g in cyclic_subgroups(C)])
--- EllipticCurves/EllipticPoint.py
+++ EllipticCurves/EllipticPoint.py
@@ -125,15 +125,3 @@ def cyclic_subgroups(C):
         Gs.append(cyclic_subgroup(P))
     
     return Gs
-        
-        
-    
-    
-C = Elliptic_Curve(2,3,223)
-P = C.points()
-a = Elliptic_Point(P[12][0],P[12][1],C)
-
-print([i.coords for i in cyclic_subgroup(a)])
-print()
-print([len(g) for g in cyclic_subgroups(C)])
-    
