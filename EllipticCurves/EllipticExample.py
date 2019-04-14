6c51e0df060bd775224d9efb8d06bf4d427c91d4
SymmetricChaos <ajfraebel@gmail.com>
2019-04-14 13:40:40 -0400

    Update EllipticExample.py
    Changed some text

--- EllipticCurves/EllipticExample.py
+++ EllipticCurves/EllipticExample.py
@@ -7,11 +7,11 @@ P = C.points()
 p = Elliptic_Point(P[8][0],P[8][1],C)
 
 
-print("The Elliptic Curve y^2 = x^3 + {}x + {} over GF({}) has {} points on it.".format(a,b,f,len(P)))
-print("Those points are:")
+print("The elliptic curve y^2 = x^3 + {}x + {} over GF({}) has {} points on it.".format(a,b,f,len(P)))
+print("\nThose points are:")
 print(P)
 
-print("\nEach of these points generates a cyclic subgroup of some order {}/n where n is a natural number.".format(len(P)))
+print("\nEach of these points generates a cyclic subgroup with an order that is a factor of {}.".format(len(P)))
 
 print("\nFor example repeated addition by {} gives the subgroup".format(P[8]))
 print([i.coords for i in cyclic_subgroup(p)])
