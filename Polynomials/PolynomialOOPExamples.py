c638424b18169bf1c148b5aea614416cc0ae164d
SymmetricChaos <ajfraebel@gmail.com>
2019-04-16 07:51:01 -0400

    Better example

--- Polynomials/PolynomialOOPExamples.py
+++ Polynomials/PolynomialOOPExamples.py
@@ -3,12 +3,12 @@ from Polynomials.PolynomialOOP import polynomial#, set_modulus, runtime_constant
 #set_modulus(9)
 #print(runtime_constants)
 
-A = polynomial([7,2,0,8],9)
-B = polynomial([10,1,9],9)
+A = polynomial([7,2,0,5],11)
+B = polynomial([12,1,3],11)
 
-print("If we write\npolynomial([7,2,0,8],9)\nthat represents")
+print("If we write\npolynomial([7,2,0,5],11)\nthat represents")
 print(A)
-print("\nIf we use a coefficient that is too large it will be reduced. So if we write\npolynomial([10,1,9],9)\nthat represents")
+print("\nIf we use a coefficient that is too large it will be reduced. So if we write\npolynomial([12,1,3],11)\nthat represents")
 print(B)
 
 print("\nWe can do a lot of familiar arithmetic using polynomials. In the following examples\nA = ",A,"\nB = ",B)
@@ -18,5 +18,5 @@ print("A + B = ",A+B)
 print("A * B = ",A*B)
 print("A / B = ",A//B)
 print("A % B = ",A%B)
-print("dx A  = ",A.derivative())
+#print("dx A  = ",A.derivative())
 
