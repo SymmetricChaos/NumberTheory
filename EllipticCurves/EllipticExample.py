acec29b2bcc13db02a2fcafe9e4ca0e5d2eae44d
SymmetricChaos <ajfraebel@gmail.com>
2019-04-17 07:52:42 -0400

    Proper __init__ file

--- EllipticCurves/EllipticExample.py
+++ EllipticCurves/EllipticExample.py
@@ -1,4 +1,4 @@
-from EllipticCurves.EllipticPoint import Elliptic_Curve, Elliptic_Point, cyclic_subgroup, cyclic_subgroups
+from EllipticCurves import Elliptic_Curve, Elliptic_Point, cyclic_subgroup, cyclic_subgroups
 
 
 a, b, f = 12, 4, 37
@@ -15,7 +15,7 @@ A = Elliptic_Point(P[10][0],P[10][1],C)
 B = Elliptic_Point(P[15][0],P[15][1],C)
 print(A,"+",B,"=",A+B)
 
-print("\nScalar multiplication, adding a point to itself repeatedly is also defined.")
+print("\nScalar multiplication, adding a point to itself repeatedly, is also defined.")
 print(A,"* 2 =", A*2)
 print(A,"* 5 =", A*5)
 
---
+++ EllipticCurves/__init__.py
@@ -0,0 +1,3 @@
+from EllipticCurves.EllipticPoint import Elliptic_Curve, Elliptic_Point, cyclic_subgroup, cyclic_subgroups
+
+__all__=["Elliptic_Curve", "Elliptic_Point", "cyclic_subgroup", "cyclic_subgroups"]
