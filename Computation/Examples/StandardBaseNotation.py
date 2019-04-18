aba4cbaa6ee353053ae8c669965c8a272b98f5b3
SymmetricChaos <ajfraebel@gmail.com>
2019-04-18 18:47:32 -0400

    Computation Examples

---
+++ Computation/Examples/StandardBaseNotation.py
@@ -0,0 +1,11 @@
+from Computation.BaseConvert import baseConvert
+
+print("     ","B2".rjust(5),"B3".rjust(5),"B4".rjust(5),"B5".rjust(5))
+for i in range(32):
+    t1 = "".join(str(x) for x in baseConvert(i,2))
+    t2 = "".join(str(x) for x in baseConvert(i,3))
+    t3 = "".join(str(x) for x in baseConvert(i,4))
+    t4 = "".join(str(x) for x in baseConvert(i,5))
+    t = "{}:".format(i)
+    print(t.ljust(5),t1.rjust(5),t2.rjust(5),t3.rjust(5),t4.rjust(5))
+    
