5caeab880df1841644d1da51ad73adf541032ce7
SymmetricChaos <ajfraebel@gmail.com>
2019-04-18 08:15:50 -0400

    Update Logarithms.py

--- Computation/Logarithms.py
+++ Computation/Logarithms.py
@@ -1,7 +1,46 @@
-# Logarithm by factorization method
+# It is straightforward to calculate logorithms by applying some root finding
+# method to them. However this can be difficult for large numbers. An
+# alternative is to use the property that if log(ab) = log(a) + log(b). If the
+# logarithms of the factors of an integer are known it is easy to find the
+# logarithm of the number. Since the logarithm function increases slowly there
+# is not much loss of accuracy from simply finding a nearby smooth number and
+# factoring it.
 
 
 from math import log2, ceil, sqrt
+
+# A simple method for 
+def simple_root(f,val,tol=.01,max_iter=10000):
+    """When does f(x) = val?"""
+    x = val
+    s = x/2
+    for i in range(max_iter):
+        
+        if abs(f(x)-val) < tol:
+            return x
+        
+        if f(x) > val:
+            x -= s
+            if f(x) < val:
+                s = s/2
+            
+        if f(x) < val:
+            x += s
+            if f(x) > val:
+                s = s/2
+
+    return x
+
+def log_basis(B):
+    
+    f = lambda x: 2**x
+    
+    D = {}
+    for b in B:
+        D[b] = simple_root(f,b,tol=.001)
+        
+    return D
+
 
 def prime_factorization(n):
     """Prime Factors with Multiplicity"""
@@ -33,7 +72,9 @@ def find_smooth(n,B):
             return P
         n -= 1
 
-def estimate_log(n,B=[2,3,5,7,11,13,17,19]):
+
+
+def estimate_log(n,D,B):
     
     D = {}
     for b in B:
@@ -43,6 +84,10 @@ def estimate_log(n,B=[2,3,5,7,11,13,17,19]):
     print(S)
     return sum(D[s] for s in S)
 
-N = 765
-print(estimate_log(N))
-print(log2(N))
+
+B = [2,3,5,7,11,13,17,19]
+D = log_basis(B)
+for N in range(800,1500,37):
+    print(estimate_log(N,D,B))
+    print(log2(N))
+    print()
