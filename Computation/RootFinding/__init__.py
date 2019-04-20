3dc6f740fe62acf2216aff8fcce563e79ef48d31
SymmetricChaos <ajfraebel@gmail.com>
2019-04-20 08:55:58 -0400

    Update __init__.py

--- Computation/RootFinding/__init__.py
+++ Computation/RootFinding/__init__.py
@@ -1,6 +1,7 @@
-from RootFinding.HalleysMethod import halleys_method, halleys_method_convergents
-from RootFinding.NewtonsMethod import newtons_method, newtons_method_convergents
-from RootFinding.SchoolyardMethod import schoolyard_method, schoolyard_method_convergents
-
+from Computation.RootFinding.HalleysMethod import halleys_method, halleys_method_convergents
+from Computation.RootFinding.NewtonsMethod import newtons_method, newtons_method_convergents
+from Computation.RootFinding.SchoolyardMethod import schoolyard_method, schoolyard_method_convergents
+from Computation.RootFinding.SquareRoots import estimate_root, babylonian_root, int_root, is_square
 __all__=["halleys_method", "halleys_method_convergents", "newtons_method", 
-         "newtons_method_convergents", "schoolyard_method", "schoolyard_method_convergents"]
+         "newtons_method_convergents", "schoolyard_method", "schoolyard_method_convergents",
+         "estimate_root", "babylonian_root", "int_root", "is_square"]
