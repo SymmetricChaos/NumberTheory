0600ad2e7f451405ac03cf69758acbae45a6e893
SymmetricChaos <ajfraebel@gmail.com>
2019-04-22 07:31:52 -0400

    Still working on it

--- Polynomials/PolynomialFactoring.py
+++ Polynomials/PolynomialFactoring.py
@@ -1,21 +1,20 @@
-from sympy import Poly, symbols
-
 from PolynomialOOP import polynomial
 from math import sqrt, floor
 from itertools import product as cart_product
 
-Q = polynomial([-20,0,1,0,3])
-print(Q)
 
-# If we can find a root of the polynomial that corresponds to a factor of the 
-# form x - n where n is the root.
-def factor_at_zero(P):
-    n = len(P)
-    for i in range(0,(n//2)+1):
-        print(i)
-        print(P.evaluate(i))
-        if P.evaluate(i)[0] == 0:
-            return polynomial([-i,1])
+# Flatten a list
+def flatten(L):
+    flat = []
+    for sublist in L:
+        try: 
+            iter(sublist)
+        except TypeError:
+            flat.append(sublist)
+        else:
+            for item in sublist:
+                flat.append(item)
+    return flat
 
 # Factors of the number that results from evalutating a polynomial at a point
 def point_factors(P,point):
@@ -31,38 +30,57 @@ def point_factors(P,point):
             
     return list(out)
 
-def flatten(L):
-    flat = []
-    for sublist in L:
-        try: 
-            iter(sublist)
-        except TypeError:
-            flat.append(sublist)
-        else:
-            for item in sublist:
-                flat.append(item)
-    return flat
-
-#R = factor_at_zero(Q)
-#S = Q//R
-#print(Q)
-#print(R)
-#print(S)
-#print(S*R)
-
-PF = point_factors(Q,0)
+# If we can find a root of the polynomial that corresponds to a factor of the 
+# form x - n where n is the root.
+def factor_at_root(P):
+    n = len(P)
+    for i in range(0,(n//2)+1):
+        if P.evaluate(i)[0] == 0:
+            return polynomial([-i,1])
+    return P
 
 
 def factor_at_nonzero(P):
     n = len(P)
     Z = point_factors(P,0)
-    print(n//2+1)
-    for i in range(1, n//2 + 1):
+    for i in range(2, n//2 + 1):
         M = point_factors(P,i)
         Z = [flatten(z) for z in cart_product(Z,M)]
         for u in Z:
             Q = polynomial(u)
-            if P//Q == 0:
+            S = P//Q
+            if len(S) == i and S.coef[-1] == 0:
                 return Q
+    return P
     
-factor_at_nonzero(Q)
+
+def poly_factor_1L(P):
+    Q = factor_at_root(P)
+    if Q == P:
+        Q = factor_at_nonzero(P)
+    return Q
+
+#def poly_factor(P):
+#    F = []
+#    Q = poly_factor_1L(P)
+#    if Q == P:
+#        return [P]
+#    
+#    F.append(Q)
+#    F.append(P//Q)
+#    
+#    
+##    out = []
+##    while True:
+##        R = F.pop()
+##        r = poly_factor_1L(P)
+##        if  ==:
+            
+            
+        
+Q = polynomial([-20,5,1,3,2])
+print(Q)
+F = poly_factor_1L(Q)
+print(F)
+print(Q//F)
+print(F*(Q//F))
