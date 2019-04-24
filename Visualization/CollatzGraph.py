8cfe6dce284fc49b1fa92a5d1277faff05b17929
SymmetricChaos <ajfraebel@gmail.com>
2019-04-24 07:47:02 -0400

    Create CollatzGraph.py

---
+++ Visualization/CollatzGraph.py
@@ -0,0 +1,9 @@
+from Other.Collatz import collatz
+import matplotlib.pyplot as plt
+
+init = 1121
+L = [c for c in collatz(init)]
+N = [i for i in range(len(L))]
+
+plt.bar(N,L)
+plt.title("The Hailstone Sequence Starting at {}".format(init))
