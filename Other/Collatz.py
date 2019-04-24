4e18009bbe3b372e88912599924d95b85955d405
SymmetricChaos <ajfraebel@gmail.com>
2019-04-24 07:46:55 -0400

    Update Collatz.py

--- Other/Collatz.py
+++ Other/Collatz.py
@@ -1,15 +1,4 @@
-# Flatten a list
-def flatten(L):
-    flat = []
-    for sublist in L:
-        try: 
-            iter(sublist)
-        except TypeError:
-            flat.append(sublist)
-        else:
-            for item in sublist:
-                flat.append(item)
-    return flat
+from GeneralUtils import flatten
 
 def collatz(n):
     while n > 1:
