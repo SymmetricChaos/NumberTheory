efd8ff7f9ad4fc54e82e0cd6dc3a5bb7574f823e
SymmetricChaos <ajfraebel@gmail.com>
2019-04-24 07:36:30 -0400

    Collatz

---
+++ Other/Collatz.py
@@ -0,0 +1,39 @@
+# Flatten a list
+def flatten(L):
+    flat = []
+    for sublist in L:
+        try: 
+            iter(sublist)
+        except TypeError:
+            flat.append(sublist)
+        else:
+            for item in sublist:
+                flat.append(item)
+    return flat
+
+def collatz(n):
+    while n > 1:
+        yield n
+        if n % 2 == 0:
+            n = n//2
+        else:
+            n = 3*n+1
+    yield n
+
+def collatz_inv(n):
+    if n % 6 == 4:
+        return 2*n, (n-1)//3
+    return 2*n
+
+def collatz_inv_graph(n,levels):
+    S = [n]
+    for i in range(levels):
+        S = [collatz_inv(m) for m in S]
+        S = flatten(S)
+        print(S)
+
+#for i in collatz(119):
+#    print(i)
+
+
+#collatz_inv_graph(1,9)
--- Other/__init__.py
+++ Other/__init__.py
@@ -1,3 +1,4 @@
 from Other.Factorization import factorization, prime_factorization
 from Other.Hyperoperation import hyper
-__all__=["factorization","prime_factorization","hyper"]
+from Other.Collatz import collatz
+__all__=["factorization","prime_factorization","hyper","collatz"]
