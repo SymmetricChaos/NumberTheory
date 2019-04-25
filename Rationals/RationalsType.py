7e2b3b9964c6c53e226f32e5b07d7804e8989617
SymmetricChaos <ajfraebel@gmail.com>
2019-04-25 08:02:46 -0400

    More operations
    Division by zero bugfix

--- Rationals/RationalsType.py
+++ Rationals/RationalsType.py
@@ -5,6 +5,7 @@ class Rational:
     def __init__(self,n,d=1):
         assert type(n) == int, "Numerator must be int."
         assert type(d) == int, "Denominator must be int."
+        assert d != 0, "Division by zero error."
         if d < 0:
             d = abs(d)
             n = -n
@@ -28,6 +29,9 @@ class Rational:
 
 
     def __str__(self):
+        return str(self.n) + "/" + str(self.d)
+    
+    def __repr__(self):
         return str(self.n) + "/" + str(self.d)
 
 
@@ -72,4 +76,29 @@ class Rational:
             if self.d == other.d:
                 return True
         return False
+    
+    
+    def __pow__(self,power):
+        assert type(power) == int,"Only powers of integers supported"
+        if power == 0:
+            return Rational(1)
+        if power == 1:
+            return self
+        else:
+            n = self.n**power
+            d = self.d**power
+            return Rational(n,d)
+
+
+    def fractional_part(self):
+        return Rational(self.n % self.d, self.d)
+    
+    
+    def mixed_form(self):
+        w = self.n // self.d
+        f = self.fractional_part()
+        return w,f
+    
+#    def egyption_form(self):
+#        pass
     
