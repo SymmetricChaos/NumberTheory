7527f90fd75e7d7b3f5659d75a78cdb460f35812
SymmetricChaos <ajfraebel@gmail.com>
2019-04-25 07:16:45 -0400

    Update RationalsType.py

--- Rationals/RationalsType.py
+++ Rationals/RationalsType.py
@@ -1,31 +1,37 @@
-from ModularArithmetic import gcd, lcm
+from ModularArithmetic import gcd
 
 class Rational:
     
     def __init__(self,n,d=1):
+        assert type(n) == int
+        assert type(d) == int
+        if d < 0:
+            d = abs(d)
+            n = -n
         self.n = n
         self.d = d
         self.simplify()
-    
+
+
     def simplify(self):
         g = abs(gcd(self.n,self.d))
         self.n = self.n//g
         self.d = self.d//g
-    
+
+
     def inv(self):
         return Rational(self.d,self.n)
-    
+
+
     def __neg__(self):
         return Rational(-self.n,self.d)
-    
-    
+
+
     def __str__(self):
         return str(self.n) + "/" + str(self.d)
-    
-    
+
+
     def __mul__(self,multiplier):
-        if type(multiplier) == int:
-            multiplier = Rational(multiplier)
         
         n = self.n * multiplier.n
         d = self.d * multiplier.d
@@ -37,15 +43,15 @@ class Rational:
         return self*multiplier
 
 
+    def __truediv__(self,divisor):
+        return self*divisor.inv()
+
+
     def __add__(self,addend):
-        if type(addend) == int:
-            addend = Rational(addend)
         
-        L = lcm(self.d,addend.d)
-        
-        n = (L//self.d)*self.n + (L//addend.d)*addend.n
-        d = L
-        
+        n = self.n*addend.d + addend.n*self.d
+        d = self.d*addend.d
+
         return Rational(n,d)
 
 
@@ -59,8 +65,15 @@ class Rational:
 
     def __rsub__(self,addend):
         return addend + -self
+
+
+    def __eq__(self,other):
+        if self.n == other.n:
+            if self.d == other.d:
+                return True
+        return False
     
-A = Rational(4,10)
+A = Rational(4,-10)
 B = Rational(7,16)
 print(A)
 print(B)
@@ -69,3 +82,7 @@ print(A-A)
 print(A*B)
 print(-A)
 print(A-B)
+print(A==A)
+print(A==B)
+print(A.inv()+A)
+print(A/B)
