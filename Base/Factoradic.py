afa9ef85f768a4af7d1642693c2925752c29b829
SymmetricChaos <ajfraebel@gmail.com>
2019-04-26 07:48:58 -0400

    Create Factoradic.py

---
+++ Base/Factoradic.py
@@ -0,0 +1,11 @@
+def factoradic(n):
+    L = []
+    ctr = 1
+    while n > 0:
+        n, l = divmod(n,ctr)
+        L.append(l)
+        ctr += 1
+    L.reverse()
+    return L
+
+print(factoradic(915467))
