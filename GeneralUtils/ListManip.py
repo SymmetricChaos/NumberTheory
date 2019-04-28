f0d432101256328932440ac1e931567c59dd792a
SymmetricChaos <ajfraebel@gmail.com>
2019-04-28 12:47:33 -0400

    Equal spacing

--- GeneralUtils/ListManip.py
+++ GeneralUtils/ListManip.py
@@ -10,3 +10,17 @@ def flatten(L):
             for item in sublist:
                 flat.append(item)
     return flat
+
+def equal_spacing(L,w,justify="right"):
+    s = ""
+    if justify == "right":
+        for i in L:
+            s += f"{i:>{w}}"
+    if justify == "left":
+        for i in L:
+            s += f"{i:<{w}}"
+    else:
+        raise Exception("Justify must be left or right.")
+    print(s)
+            
+equal_spacing([1,23,456],6)
