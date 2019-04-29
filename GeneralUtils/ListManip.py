ee06c465de16562aa9a4ae310120dff3029f5d83
SymmetricChaos <ajfraebel@gmail.com>
2019-04-29 07:43:41 -0400

    Update ListManip.py

--- GeneralUtils/ListManip.py
+++ GeneralUtils/ListManip.py
@@ -1,6 +1,5 @@
-# Flatten a list
 def flatten(L):
-    """Turn a list of iterable into a single list of their elements"""
+    """Turn a list of iterables and non-iterables into a single list of their elements"""
     flat = []
     for sublist in L:
         try: 
@@ -31,3 +30,20 @@ def equal_spacing_grid(L,w,justify="right"):
     """Print a list of iterables into a grid"""
     for r in L:
         equal_spacing(r,w,justify)
+        
+        
+def lists_to_tuples(*args):
+    """Convert lists to a list of tuples"""
+    L = []
+    for i in zip(*args):
+        L.append(i)
+    return L
+
+
+def tuples_to_lists(L):
+    """Convert a list of tuples to lists"""
+    out = []
+    W = len(L[0])
+    for w in range(W):
+        out.append( [i[w] for i in L] )
+    return out
