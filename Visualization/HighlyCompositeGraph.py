a87240171d2523ed04a1bdd9f47fdc6ca71a3aa8
SymmetricChaos <ajfraebel@gmail.com>
2019-04-29 07:44:01 -0400

    Update HighlyCompositeGraph.py

--- Visualization/HighlyCompositeGraph.py
+++ Visualization/HighlyCompositeGraph.py
@@ -5,7 +5,7 @@ N = []
 L = []
 Hn = [2]
 Hl = [2]
-for i in range(2,1700):
+for i in range(2,1000):
     f = len(factorization(i))
     N.append(i)
     L.append(f)
@@ -17,6 +17,6 @@ for i in range(2,1700):
 print(Hn)
 
 fig = plt.figure()
-fig.set_size_inches(13, 6)
-plt.bar(N,L,3)
+fig.set_size_inches(15, 6)
+plt.bar(N,L,2)
 plt.scatter(Hn,Hl,zorder=2)
