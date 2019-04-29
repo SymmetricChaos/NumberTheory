bb40bb2588c642c6c44c95be85e71245025e4f0f
SymmetricChaos <ajfraebel@gmail.com>
2019-04-29 13:52:42 -0400

    Create ContinuedFractions.py

---
+++ Rationals/ContinuedFractions.py
@@ -0,0 +1,16 @@
+from RationalsType import Rational
+
+def cfrac(L):
+    a,b = L[-1],1
+    del L[-1]
+    for i in reversed(L):
+        A = Rational(i,1)
+        B = Rational(b,a)
+        C = A+B
+        a = C.n
+        b = C.d
+    return Rational(a,b)
+
+
+print(cfrac([1,1,1,1,1,1,1,1,1,1]))
+print(89/55)
