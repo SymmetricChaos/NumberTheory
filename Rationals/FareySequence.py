14263b1f3546c482bef18b21f20fb160c4e9d79b
SymmetricChaos <ajfraebel@gmail.com>
2019-05-01 21:38:48 -0400

    Update FareySequence.py
    Assertions added

--- Rationals/FareySequence.py
+++ Rationals/FareySequence.py
@@ -1,6 +1,9 @@
 from RationalsType import Rational
 
 def farey_sequence(n):
+    assert type(n) == int
+    assert n > 0
+        
     S = [Rational(0,1), Rational(1,n)]
     while True:
         A = S[-2]
@@ -12,6 +15,7 @@ def farey_sequence(n):
             break
         S.append(Rational(p,q))
     return S
-        
-F = farey_sequence(9)
-print(F)
+
+for i in range(1,7):
+    F = farey_sequence(i)
+    print(F)
