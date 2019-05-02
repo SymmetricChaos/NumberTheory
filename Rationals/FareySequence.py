3a14fbb7e2024a2af570ef4c871ae0e5d12d70e2
SymmetricChaos <ajfraebel@gmail.com>
2019-05-01 20:58:51 -0400

    Update FareySequence.py
    More efficient calculation

--- Rationals/FareySequence.py
+++ Rationals/FareySequence.py
@@ -1,15 +1,17 @@
 from RationalsType import Rational
 
 def farey_sequence(n):
-    S = set()
-    for a in range(n):
-        for b in range(1,n+1):
-            R = Rational(a,b)
-            if R < 1:
-                S.add(R)
-    S = list(S)
-    S.sort()
+    S = [Rational(0,1), Rational(1,n)]
+    while True:
+        A = S[-2]
+        B = S[-1]
+        k = (n + A.d) // B.d
+        p = k * B.n-A.n
+        q = k * B.d-A.d
+        if p > n:
+            break
+        S.append(Rational(p,q))
     return S
         
-F = farey_sequence(8)
+F = farey_sequence(9)
 print(F)
