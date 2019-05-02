28f87c340b01b05a7e32ec8fe6a8f5f195d58342
SymmetricChaos <ajfraebel@gmail.com>
2019-05-01 21:54:35 -0400

    Create HarmonicSequence.py

---
+++ Rationals/HarmonicSequence.py
@@ -0,0 +1,12 @@
+from RationalsType import Rational
+
+def harmonic_sequence(n):
+    H = Rational(1,1)
+    ctr = 2
+    while True:
+        yield H
+        if ctr > n:
+            break
+        H += Rational(1,ctr)
+        ctr += 1
+        
