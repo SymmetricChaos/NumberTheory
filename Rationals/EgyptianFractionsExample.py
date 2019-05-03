d7b9635f507b2b91d2e3629b8cf658285c9024cb
SymmetricChaos <ajfraebel@gmail.com>
2019-05-03 19:44:59 -0400

    Update EgyptianFractionsExample.py
    Examples using the splitting method

--- Rationals/EgyptianFractionsExample.py
+++ Rationals/EgyptianFractionsExample.py
@@ -1,27 +1,38 @@
-from EgyptianFractions import egyptian_form_factoring, egyptian_split
+from EgyptianFractions import egyptian_form_factoring, \
+                              egyptian_form_splitting
 from RationalsType import Rational
 import random
 
 def list_to_sum(L):
     return " + ".join(str(l) for l in L)
 
+
 print("Splitting Method")
 for i in range(5):
-    print()
-    D = random.randint(3,100)
-    A = Rational(2,D)
-    E = egyptian_split(D)
-    print(A," = ",list_to_sum(E))
+
     
-    if sum(E) != A:
-        raise Exception("SUM IS WRONG")
+    D = random.randint(2,15)
+    N = random.randint(2,D)
+    
+    A = Rational(N,D)
+    if A.n == 1:
+        continue
+    else:
+        print()
+    
+        E = egyptian_form_splitting(A)
+        print(A," = ",list_to_sum(E))
+        
+        if sum(E) != A:
+            raise Exception("SUM IS WRONG")
 
 
 print("\n\nFactoring Method")
 ctr = 0
 while ctr < 5:
-    N = random.randint(2,100)
+    
     D = random.randint(2,100)
+    N = random.randint(2,D)
     
     A = Rational(N,D)
     if A.n == 1:
@@ -36,3 +47,4 @@ while ctr < 5:
         print(A," = ", list_to_sum(E))
         if sum(E) != A:
             raise Exception("SUM IS WRONG")
+            
