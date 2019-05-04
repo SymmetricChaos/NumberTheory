47fa188e477fa0d4c2b2422d3c80fd5fe63da3b4
SymmetricChaos <ajfraebel@gmail.com>
2019-05-03 22:38:15 -0400

    Update EgyptianFractionsExample.py
    Function to make examples

--- Rationals/EgyptianFractionsExample.py
+++ Rationals/EgyptianFractionsExample.py
@@ -6,45 +6,30 @@ import random
 def list_to_sum(L):
     return " + ".join(str(l) for l in L)
 
+def egyptian_example(func):
+    ctr = 0
+    while ctr < 5:
+        D = random.randint(2,50)
+        N = random.randint(2,D)
+        
+        A = Rational(N,D)
+        
+        if A.n == 1:
+            continue
+        
+        else:
+            E = func(A)
+            if E != []:
+                ctr += 1
+                print()
+                print(A," = ", list_to_sum(E))
+                if sum(E) != A:
+                    raise Exception("SUM IS WRONG")
+
 
 print("Splitting Method")
-for i in range(5):
-
-    
-    D = random.randint(2,15)
-    N = random.randint(2,D)
-    
-    A = Rational(N,D)
-    if A.n == 1:
-        continue
-    else:
-        print()
-    
-        E = egyptian_form_splitting(A)
-        print(A," = ",list_to_sum(E))
-        
-        if sum(E) != A:
-            raise Exception("SUM IS WRONG")
+egyptian_example(egyptian_form_splitting)
 
 
 print("\n\nFactoring Method")
-ctr = 0
-while ctr < 5:
-    
-    D = random.randint(2,100)
-    N = random.randint(2,D)
-    
-    A = Rational(N,D)
-    if A.n == 1:
-        continue
-    E = egyptian_form_factoring(A)
-    
-    if E == []:
-        continue
-    else:
-        ctr += 1
-        print()
-        print(A," = ", list_to_sum(E))
-        if sum(E) != A:
-            raise Exception("SUM IS WRONG")
-            
+egyptian_example(egyptian_form_factoring)
