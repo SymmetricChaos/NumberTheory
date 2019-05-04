d5ffc535ba55b547ab8d143b23de1a84aebaeaae
SymmetricChaos <ajfraebel@gmail.com>
2019-05-04 15:15:37 -0400

    Create FactorialExample.py

---
+++ Computation/Examples/FactorialExample.py
@@ -0,0 +1,6 @@
+from Computation.RisingAndFallingFactorial import falling_factorial, rising_factorial
+
+x = 5
+n = 5
+print(f"falling_fac({x},{n}) = {falling_factorial(x,n)}")
+print(f"rising_fac({x},{n}) = {rising_factorial(x,n)}")
