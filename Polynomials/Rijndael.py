0d9fcec0f909204612810d5739398eb53ae52520
SymmetricChaos <ajfraebel@gmail.com>
2019-05-04 18:09:00 -0400

    Update Rijndael.py
    Now uses a class containing a polynomial

--- Polynomials/Rijndael.py
+++ Polynomials/Rijndael.py
@@ -2,30 +2,44 @@ from PolynomialType import Polynomial
 
 # The finite field GF(2^8) with 256 elements has various expressions
 
-def rij_print(P):
-    s = ""
-    for i in reversed(P.coef):
-        s += str(i)
-    print(s)
+# The Rijndael polynomial 
+R = Polynomial([1,1,0,1,1,0,0,0,1],2)
+class Rijndael:
+    
+    def __init__(self,coef):
+        assert len(coef) < 9
+        self.poly = Polynomial(coef,modulus=2)
+    
+    def __add__(self,Q):
+        assert type(self) == Rijndael
+        assert type(Q) == Rijndael
         
-
-def rij_add(P,Q):
+        a = (self.poly+Q.poly) % R
+        return Rijndael(a.coef)
     
-    # The Rijndael polynomial 
-    R = Polynomial([1,1,0,1,1,0,0,0,1],2)
+    def __mul__(self,Q):
+        assert type(self) == Rijndael
+        assert type(Q) == Rijndael
+        
+        a = (self.poly*Q.poly) % R
+        return Rijndael(a.coef)
     
-    return (P+Q) % R
-
-def rij_mult(P,Q):
+    def __str__(self):
+        c = [str(i) for i in reversed(self.poly.coef)]
+        c = "".join(c)
+        return f"{c:>8}"
     
-    # The Rijndael polynomial 
-    R = Polynomial([1,1,0,1,1,0,0,0,1],2)
+    def __repl__(self):
+        s = ""
+        for i in reversed(self.poly.coef):
+            s += str(i)
+        return s
     
-    return (P*Q) % R
 
-P = Polynomial([0,1,1,0,1,1,1],2)
-Q = Polynomial([1,0,1,0,1],2)
 
-rij_print(P)
-rij_print(Q)
-rij_print(rij_add(P,Q))
+P = Rijndael([0,1,1,0,1,1,1,1])
+Q = Rijndael([1,0,1,0,1])
+
+print(P)
+print(Q)
+print(P+Q)
