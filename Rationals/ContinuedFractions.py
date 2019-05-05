7df9ac3791cdac9461002b4b26e41cb1a44fc2c8
SymmetricChaos <ajfraebel@gmail.com>
2019-05-05 10:59:37 -0400

    Update ContinuedFractions.py
    More efficient calculation of convergents

--- Rationals/ContinuedFractions.py
+++ Rationals/ContinuedFractions.py
@@ -12,6 +12,12 @@ def cfrac(L):
     return Rational(a,b)
 
 def cfrac_convergents(L):
-    
-    for i in range(1,len(L)):
-        yield cfrac(L[:i])
+    N = [1,L[0]]
+    D = [0,1]
+    con = 2
+    while con < len(L):
+        yield Rational(N[-1],D[-1])
+        N.append( L[con] * N[con-1] + N[con-2] )
+        D.append( L[con] * D[con-1] + D[con-2] )
+        con += 1
+    yield Rational(N[-1],D[-1])
