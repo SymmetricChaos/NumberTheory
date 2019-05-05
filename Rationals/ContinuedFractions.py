7b235b82ee4d75ec3c880b24072fceccfbe7c995
SymmetricChaos <ajfraebel@gmail.com>
2019-05-05 11:45:03 -0400

    Update ContinuedFractions.py
    Function that created a continued fraction representation of a rational.

--- Rationals/ContinuedFractions.py
+++ Rationals/ContinuedFractions.py
@@ -15,9 +15,26 @@ def cfrac_convergents(L):
     N = [1,L[0]]
     D = [0,1]
     con = 2
-    while con < len(L):
-        yield Rational(N[-1],D[-1])
-        N.append( L[con] * N[con-1] + N[con-2] )
-        D.append( L[con] * D[con-1] + D[con-2] )
-        con += 1
+    
     yield Rational(N[-1],D[-1])
+    
+    while con < len(L)+1:
+        N.append( L[con-1] * N[con-1] + N[con-2] )
+        D.append( L[con-1] * D[con-1] + D[con-2] )
+
+        yield Rational(N[-1],D[-1])
+        
+        con += 1
+        
+def cfrac_expansion(rational):
+    assert type(rational) == Rational
+    
+    L = []
+    while True:
+        w,f = rational.mixed_form()
+        L.append(w)
+        if f == 0:
+            break
+        rational = f.inv()
+
+    return L
