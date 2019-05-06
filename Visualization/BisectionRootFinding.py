f6ca093623a191774f38302b40f36e8a03a91617
SymmetricChaos <ajfraebel@gmail.com>
2019-05-06 19:15:15 -0400

    Update BisectionRootFinding.py
    Better visualization

--- Visualization/BisectionRootFinding.py
+++ Visualization/BisectionRootFinding.py
@@ -4,12 +4,12 @@ from numpy import linspace
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-fig.set_size_inches(10, 6)
+fig.set_size_inches(12, 6)
 
 P = Polynomial([-2,-1,0,1])
 print(P)
 
-x = linspace(-0,2,30)
+x = linspace(0,2,30)
 y = P.evaluate(x)[0]
 
 def poly_eval(x):
@@ -17,9 +17,11 @@ def poly_eval(x):
 
 plt.plot(x,y)
 plt.axhline(0)
-con = bisection_method_convergents(0,2,poly_eval,6)
+con = bisection_method_convergents(0,2,poly_eval,10)
+prev = 0
 for ctr,pos in enumerate(con):
-    plt.scatter(pos,0,zorder=3,color='black')
-    plt.text(pos-.01,.15,str(ctr+1))
+    plt.scatter(pos,poly_eval(pos),zorder=3,color='black')
+    plt.plot([prev,pos],[4-ctr*.2,4-ctr*.2],color='black')
     print(pos)
     
+    prev = pos
