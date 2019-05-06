478cb72e4084452615dbcf41311c2aab908ed750
SymmetricChaos <ajfraebel@gmail.com>
2019-05-06 17:42:42 -0400

    Sign function

--- GeneralUtils/__init__.py
+++ GeneralUtils/__init__.py
@@ -1,2 +1,3 @@
 from GeneralUtils.ListManip import flatten, equal_spacing, equal_spacing_grid, list_to_sum
-__all__=["flatten","equal_spacing","equal_spacing_grid","list_to_sum"]
+from GeneralUtils.Math import sign
+__all__=["flatten","equal_spacing","equal_spacing_grid","list_to_sum","sign"]
