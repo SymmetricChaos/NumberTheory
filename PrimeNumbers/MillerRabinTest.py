1716a720095277c904ca9deec6638dce7690ea21
SymmetricChaos <ajfraebel@gmail.com>
2019-05-08 10:08:06 -0400

    Update MillerRabinTest.py
    Comments, warning restored

--- PrimeNumbers/MillerRabinTest.py
+++ PrimeNumbers/MillerRabinTest.py
@@ -1,20 +1,21 @@
-## The Miller-Rabin primality test is an extremely fast method of checking
-## whether or not a number is prime. When it returns False the number is
-## definitely composite. When it returns True the number is probably prime.
-## However with the default set of witnesses this is deterministic only for
-## number up to 2^80
+# The Miller-Rabin primality test is an extremely fast method of checking
+# whether or not a number is prime. When it returns False the number is
+# definitely composite. When it returns True the number is probably prime.
+# However with the default set of witnesses this is deterministic only for
+# number up to 2^80
 
-## Test if a number is composite for a given base
+# Test if a number is composite for a given base
 def is_composite(a,d,n,r):
     for i in range(0,r+1):
         if pow(a,2**i*d,n) == 1:
             return False
     return True
 
-## Wrapper function
+# Wrapper function
 def miller_rabin_test(n,W=[2,3,5,7,11,13,17,19,23,29,31,37,41]):
     """Miller-Rabin primality test"""
-    ## Deal with even numbers first
+    
+    # Deal with even numbers first
     if n == 2:
         return True
     if n % 2 == 0:
@@ -29,12 +30,12 @@ def miller_rabin_test(n,W=[2,3,5,7,11,13,17,19,23,29,31,37,41]):
     for witness in W:
         if witness >= n:
             break
-        ## If the test for compositeness returns true we know for certain the
-        ## number is composite and we return false
+        # If the test for compositeness returns true we know for certain the
+        # number is composite and we return false
         if is_composite(witness,d,n,r):
             return False
     
-    #if n > (2**81):
-    #    print("Test is only probable.")
+    if n > (2**80):
+        print("Test is only probable.")
         
     return True
