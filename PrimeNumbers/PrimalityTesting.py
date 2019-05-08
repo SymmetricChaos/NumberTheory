6ad457adfab79f8595daad43b824bacea14ab492
SymmetricChaos <ajfraebel@gmail.com>
2019-05-08 10:08:30 -0400

    Update PrimalityTesting.py
    Example of trial division and fermat with trial

--- PrimeNumbers/PrimalityTesting.py
+++ PrimeNumbers/PrimalityTesting.py
@@ -1,8 +1,10 @@
 from time import time
+from random import sample
 
-from PrimeNumbers.MillerRabinTest import miller_rabin_test
-from PrimeNumbers.LucasLehmerTest import lucas_lehmer_test
-from PrimeNumbers.TrialFactorization import 
+#from PrimeNumbers.MillerRabinTest import miller_rabin_test
+#from PrimeNumbers.LucasLehmerTest import lucas_lehmer_test
+from PrimeNumbers.TrialDivision import trial_division_test, fermat_and_trial_test
+from PrimeNumbers.Primes import primes
 
 #print("Lets try checking some Mersenne numbers for primality!")
 #from LucasLehmer import LLPrimality
@@ -17,6 +19,31 @@ from PrimeNumbers.TrialFactorization import
 #        
 
 
-for i in range(3,110):
-    if lucas_lehmer_test(i):
-        print(f"2^{i}-1 = {2**i-1}")
+#for i in range(3,110):
+#    if lucas_lehmer_test(i):
+#        print(f"2^{i}-1 = {2**i-1}")
+
+
+prime_list = []
+for p in primes():
+    if p > 20000000:
+        break
+    prime_list.append(p)
+print(len(prime_list))
+ctr = 0
+while ctr < 50:
+
+    a,b = sample(prime_list,2)
+    N = a*b
+    print(a,b)
+    print(N)
+    ctr += 1
+    t0 = time()
+    trial_division_test(N)
+    print(f"Trial Division {time()-t0:5f} seconds")
+    
+    t0 = time()
+    fermat_and_trial_test(N,25)
+    print(f"Fermat Trial   {time()-t0:5f} seconds")
+    
+    print()
