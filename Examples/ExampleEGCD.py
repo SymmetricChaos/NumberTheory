7bb3029b4a38664887cceaed40853432c60e40f6
SymmetricChaos <ajfraebel@gmail.com>
2019-05-10 08:08:22 -0400

    Expanded Examples

--- Examples/ExampleEGCD.py
+++ Examples/ExampleEGCD.py
@@ -1,4 +1,4 @@
-from ModularArithmetic.Utils import gcd, lcm
+from ModularArithmetic.Utils import gcd, lcm, egcd
 
 S = [629,777,407]
 print("The greatest common denominator of natural numbers is the largest number that is a factor of all of them.")
@@ -8,3 +8,12 @@ print("\n\nThe least common multiple is the smallest number that is a multiple o
 print(f"The LCM of the set {S} is {lcm(S)}")
 
 print("\nIf the GCD is known then it is easy to find the LCM as it is the product of the set divided by their GCD.")
+
+print("\n\nCalculating the GCD can be done reasonably quickly using the Euclidean Algorithm.")
+
+print("\n\nBézout's identity says that given a and b if their GCD is g then there are integers x and y such that\ng = ax + by")
+print("\nA small change to the Euclidean Algorithm returns these numbers as well.")
+a, b = 629,407
+g,x,y = egcd(a,b)
+print(f"{a*x+b*y} = {a}*{x} + {b}*{y}")
+
--- Examples/ExampleQuadRes.py
+++ Examples/ExampleQuadRes.py
@@ -1,5 +1,7 @@
 from ModularArithmetic import find_quad_residue
-    
+
+
+print("The quadratic residues of a numbers are the remainders of squares when divided by that number.")
 for i in range(2,22):
     qr = find_quad_residue(i)
     print("{:<{}} {}".format(i,2,qr))
