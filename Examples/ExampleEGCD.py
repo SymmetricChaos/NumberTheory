925cd909d3e22e90f767fb4cb8ed5ca2475c822c
SymmetricChaos <ajfraebel@gmail.com>
2019-05-10 18:43:31 -0400

    Update ExampleEGCD.py
    Show how euclid's algorithm works

--- Examples/ExampleEGCD.py
+++ Examples/ExampleEGCD.py
@@ -1,4 +1,5 @@
 from ModularArithmetic.Utils import gcd, lcm, egcd
+from GeneralUtils import equal_spacing
 
 S = [629,777,407]
 print("The greatest common denominator of natural numbers is the largest number that is a factor of all of them.")
@@ -10,6 +11,20 @@ print(f"The LCM of the set {S} is {lcm(S)}")
 print("\nIf the GCD is known then it is easy to find the LCM as it is the product of the set divided by their GCD.")
 
 print("\n\nCalculating the GCD can be done reasonably quickly using the Euclidean Algorithm.")
+
+a, b = 629,777
+print(f"\nThis algorithm procedes by subtracting the smaller number from the larger until the numbers are equal. Here we find the GCD of {a} and {b}.\n")
+equal_spacing([a,b],5,'left')
+while a != b:
+    if a > b:
+        a = a-b
+    else:
+        b = b-a
+    equal_spacing([a,b],5,'left')
+    
+print(f"\nSo the their greatest common denominator is {gcd(a,b)}")
+
+print("\nThere are slightly more efficient methods of calculation available today.")
 
 print("\n\nBézout's identity says that given a and b if their GCD is g then there are integers x and y such that\ng = ax + by")
 print("\nA small change to the Euclidean Algorithm returns these numbers as well.")
