919300a515635ca79182de562da25a1f62bbe7a4
SymmetricChaos <ajfraebel@gmail.com>
2019-05-13 14:33:32 -0400

    prod

--- GeneralUtils/Math.py
+++ GeneralUtils/Math.py
@@ -5,3 +5,9 @@ def sign(x):
         return -1
     if x == 0:
         return 0
+    
+def prod(L):
+    out = 1
+    for i in L:
+        out *= i
+    return out
--- GeneralUtils/__init__.py
+++ GeneralUtils/__init__.py
@@ -1,5 +1,5 @@
 from GeneralUtils.ListManip import flatten, equal_spacing, equal_spacing_grid, list_to_sum, list_to_prod
 from GeneralUtils.DictManip import show_dict
-from GeneralUtils.Math import sign
+from GeneralUtils.Math import sign, prod
 __all__=["flatten","equal_spacing","equal_spacing_grid","list_to_sum",
-         "list_to_prod","sign","show_dict"]
+         "list_to_prod","sign","prod","show_dict"]
