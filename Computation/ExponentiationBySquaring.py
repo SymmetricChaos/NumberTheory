6b3a7b7636dea87946c314016fc5ad43c75552a8
SymmetricChaos <ajfraebel@gmail.com>
2019-05-13 14:34:00 -0400

    Create ExponentiationBySquaring.py

---
+++ Computation/ExponentiationBySquaring.py
@@ -0,0 +1,31 @@
+from GeneralUtils import prod
+
+def binary_partition(n):
+    """Get the powers of two that sum to an integer"""
+
+    if(n == 0):
+        return([0])
+    out = []
+    ctr = 0
+    while(n > 0):
+        if n % 2 == 1:
+            out.append(ctr)
+        ctr += 1
+        n //= 2
+    return(out)
+
+
+def exp_by_squaring(b,n,m=0):
+    p = binary_partition(n)
+    #Multiplications needed
+    #print(len(p)+max(p)-1)
+    t = b
+    L = [b]
+    for i in range(p[-1]):
+        t = t*t
+        L.append(t)
+        
+    return prod([L[i] for i in p])
+
+#print(exp_by_squaring(2,471))
+#print(2**471)
