99377da173c477f0dfe6f868b625fdda775b5249
SymmetricChaos <ajfraebel@gmail.com>
2019-05-14 10:03:59 -0400

    kronecker_symbol

--- Examples/LegendreSymbolExample.py
+++ Examples/LegendreSymbolExample.py
@@ -1,22 +1,35 @@
-from ModularArithmetic import legendre_symbol, jacobi_symbol
+from ModularArithmetic import legendre_symbol, jacobi_symbol, kronecker_symbol
 from GeneralUtils import equal_spacing_grid
 
-G = [[" ",1,2,3,4,5],[]]
-for p in [3,5,7,11,13,17]:
+print("Legendre Symbol")
+G = [[" ",1,2,3,4,5,6],[]]
+for p in [3,5,7,11,13,17,19]:
     R = [f"{p} "]
-    for a in [1,2,3,4,5]:
+    for a in [1,2,3,4,5,6]:
         R.append(legendre_symbol(a,p))
     G.append(R)
 
 equal_spacing_grid(G,3)
 
-print("\n\n\n")
+print("\n\n\nJacobi Symbol")
 
-G = [[" ",1,2,3,4,5],[]]
-for p in [1,3,5,7,9,11]:
+G = [[" ",1,2,3,4,5,6],[]]
+for p in [1,3,5,7,9,11,13]:
     R = [f"{p} "]
-    for a in [1,2,3,4,5]:
+    for a in [1,2,3,4,5,6]:
         R.append(jacobi_symbol(a,p))
+    G.append(R)
+
+equal_spacing_grid(G,3)
+
+
+print("\n\n\nKronecker Symbol")
+
+G = [[" ",1,2,3,4,5,6],[]]
+for p in [1,2,3,4,5,6]:
+    R = [f"{p} "]
+    for a in [1,2,3,4,5,6]:
+        R.append(kronecker_symbol(a,p))
     G.append(R)
 
 equal_spacing_grid(G,3)
