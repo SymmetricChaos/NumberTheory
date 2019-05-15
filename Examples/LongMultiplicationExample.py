d46f11ec004ed37f3922af3c6804fbdb4a02abdc
SymmetricChaos <ajfraebel@gmail.com>
2019-05-15 18:29:33 -0400

    Update LongMultiplicationExample.py
    proper sign

--- Examples/LongMultiplicationExample.py
+++ Examples/LongMultiplicationExample.py
@@ -5,7 +5,7 @@ for i in range(3):
     A = randint(0,20000)
     B = randint(0,A)
     print(f"{A:>7}")
-    print(f"x{B:>6}\n")
+    print(f"×{B:>6}\n")
     C = long_multiplication_steps(A,B)
     if C != A*B:
         print("ERROR")
