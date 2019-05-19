f36fd42bacc9014749a8408bff71a429e7b8220d
SymmetricChaos <ajfraebel@gmail.com>
2019-05-19 10:13:27 -0400

    Additive Persistence

--- Other/DigitalSum.py
+++ Other/DigitalSum.py
@@ -1,12 +1,23 @@
 def digital_sum(n):
+    if n < 10:
+        return n
     L = [int(i) for i in str(n)]
     return sum(L)
     
 def digital_root(n):
+    if n < 10:
+        return n
     L = [int(i) for i in str(n)]
     while len(L) > 1:
         s = sum(L)
         L = [int(i) for i in str(s)]
     return s
 
-print(digital_root(23487987654376))
+def additive_persistence(n):
+    steps = 0
+    L = [int(i) for i in str(n)]
+    while len(L) > 1:
+        steps += 1
+        s = sum(L)
+        L = [int(i) for i in str(s)]
+    return steps
--- Other/__init__.py
+++ Other/__init__.py
@@ -2,4 +2,6 @@ from Other.Factorization import factorization, prime_factorization
 from Other.Hyperoperation import hyper
 from Other.Collatz import collatz
 from Other.SubsetSum import subset_sum
-__all__=["factorization","prime_factorization","hyper","collatz","subset_sum"]
+from Other.DigitalSum import digital_sum, digital_root, additive_persistence
+__all__=["factorization","prime_factorization","hyper","collatz","subset_sum",
+         "digital_sum","digital_root","additive_persistence"]
