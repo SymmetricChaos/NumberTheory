3461887bed3c4436acee791f3b6c96a8dd9da7fa
SymmetricChaos <ajfraebel@gmail.com>
2019-05-21 22:48:01 -0400

    Update SubsetSum.py

--- Other/SubsetSum.py
+++ Other/SubsetSum.py
@@ -1,10 +1,11 @@
 from itertools import chain, combinations
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """The powerset of some iterable"""
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
+# This method is inefficient but it does guarantee no shorter sum exists
 def subset_sum(L,n):
     """Find a subset of L that has a sum of n if it exists"""
     
@@ -14,39 +15,41 @@ def subset_sum(L,n):
             return i
     return ()
 
+def is_subset_sum(s,pos,n,out=[]):
+    #print(out)
+    # If we have gotten the sum down to zero than we have succeeded
+    if n == 0:
+        return out
+    
+    # If we are not considering anything and we haven't gotten to zero then
+    # this branch is a failure.
+    if n >= 0 and pos == 0:
+        return []
+    
+    # If the last element in the array is bigger than the sum then we only need
+    # to consider branches without it.
+    if (s[pos - 1] > n):
+        return is_subset_sum(s, pos - 1, n,out); 
+    
+    # Otherwise try removing the last element and try including the last
+    # element. If either branch finds a sum then we succeed.
+    # We are exploiting a clever feature of Python here. We can use many objects
+    # are inputs for logical operators. An empty list is evaluated as False while
+    # any non-empty list is evaluated as True.
+    return is_subset_sum(s,pos-1,n,out) or is_subset_sum(s, pos-1, n-s[pos-1],out+[s[pos-1]])
+
 
 def subset_sum_dynamic(L,N):
     
-    def is_subset_sum(s,n,sm):
-        print(s[:n],sm)
-        # If we are not considering anything and we haven't gotten to zero then
-        # this branch is a failure.
-        if sm >= 0 and n == 0:
-            return False
-        
-        # If we have gotten the sum down to zero than we have succeeded
-        if sm == 0:
-            return True
-        
-        # If the last element in the array is bigger than the sum try excluding
-        # the element
-        if (s[n - 1] > sm) : 
-            return is_subset_sum(s, n - 1, sm); 
-   
-        # Otherwise try removing the last element and try including the last
-        # element. If either of these branches succeeds then 
-        return is_subset_sum(s,n-1,sm) or is_subset_sum(s, n-1, sm-s[n-1])
-    
     return is_subset_sum(L,len(L),N)
     
-print(subset_sum_dynamic([1,9,21],31))
+#print(subset_sum_dynamic([5,8,40,9,17,21,1,11,19],31))
 
-
-#import random
-#for i in range(1):
-#    L = [random.randint(0,100) for i in range(5)]
-#    N = random.randint(0,1000) 
-#    S = subset_sum_dynamic(L,N)
-#    print(N,L)
-#    print(S)
-#    print()
+import random
+for i in range(5):
+    L = [random.randint(0,100) for i in range(30)]
+    N = random.randint(0,1000) 
+    print(N,L)
+    print(subset_sum_dynamic(L,N))
+    #print(subset_sum(L,N))
+    print()
