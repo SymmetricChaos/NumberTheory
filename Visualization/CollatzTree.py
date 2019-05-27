cf4195cabce43bc1eec265ac2ba6a4c564779b2c
SymmetricChaos <ajfraebel@gmail.com>
2019-05-27 07:38:53 -0400

    Update CollatzTree.py
    Better graphic

--- Visualization/CollatzTree.py
+++ Visualization/CollatzTree.py
@@ -13,8 +13,8 @@ def collatz_tree_pos(L,narrowness=2):
         return [[8,x,y+1],[1,0,0]]
     a = collatz_inv(N)
     if len(a) == 1:
-        return [[a[0],x,y+1.3],[1,0,0]]
-    return [[a[0],x-1/(y**narrowness),y+1.618], [a[1],x+1/(y**narrowness),y+1]]
+        return [[a[0],x,y+1.2],[1,0,0]]
+    return [[a[0],x-1/(y**narrowness),y+1.7], [a[1],x+1/(y**narrowness),y+1]]
 
 
 def collatz_tree(levels,narrowness=2,text=True):
@@ -35,11 +35,14 @@ def collatz_tree(levels,narrowness=2,text=True):
         S += new
     if text == True:
         for i in S:
+            plt.scatter(i[1],i[2],s=350,color='white',zorder=3)
             plt.text(i[1],i[2],str(i[0]),ha='center',va='center',fontsize=15-floor(log(i[0],10)))
         
         
 fig = plt.figure()
 fig.set_size_inches(40, 20)
+ax=fig.add_axes([0,0,1,1])
+ax.set_axis_off()
 collatz_tree(14,2.5,True)
 
 #for i in collatz_inv_graph(1,10):
