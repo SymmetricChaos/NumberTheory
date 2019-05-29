3a31e26d9001c357f7c759c3223a18ec324194c0
SymmetricChaos <ajfraebel@gmail.com>
2019-05-29 11:56:21 -0400

    Update AdditionChain.py

--- Computation/AdditionChain.py
+++ Computation/AdditionChain.py
@@ -2,35 +2,37 @@
 # This is a simple branch and bound style algorithm which starts by trying
 # the greedy method and prunes branches that are worse than what it has found
 # before.
-def addition_chain(N,L=[1],best=float('inf')):
 
+def addition_chain(N,L=[1],best=float('inf')):
     # If we found a solution return it
     if L[-1] == N:
         return L
     
-    # If we're not better than the best known terminate by returning a wrong
-    # answer without further searching.
+    # If we're not better than the best known terminate by returning None
     if len(L) >= best:
-        return L+[0]
+        return None
     
-    # Otherwise search all valid steps to continue and make a list of the
-    # results.
-    # Then return the shortest of those.
-    
-    t = []
-    
+    # Otherwise search all valid steps to continue
+    out = []
     for i in reversed(L):
         d = (L[-1]+i)
         
+        # Ignore branches that would give too large of a number
         if N - d >= 0:
             
             v = addition_chain(N,L+[d],best)
+            # If we get a None then the branch failed and we add nothing to the
+            # list of options
+            if v == None:
+                continue
+            # Otherwise we have a new best solution
             if len(v) < best:
                 best = len(v)
-            t.append(v)
-    print(min(t,key=len)) 
-    return min(t,key=len)
-            
-v = addition_chain(16)
-print()
-print(v)
+                out = v
+    
+    # If all branches returned none then this whole section is ignored
+    if out == []:
+        return None
+    return out
+
+print(addition_chain(31))
