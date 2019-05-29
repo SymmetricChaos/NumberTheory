b48ffbeaa1d0750dac811d3269cbefae45fe3671
SymmetricChaos <ajfraebel@gmail.com>
2019-05-29 08:28:53 -0400

    Exp by Square Example

--- Computation/__init__.py
+++ Computation/__init__.py
@@ -1,3 +1,5 @@
 from Computation.LongDivision import long_division
 from Computation.LongMultiplication import long_multiplication, long_multiplication_steps
-__all__=["long_division","long_multiplication","long_multiplication_steps"]
+from Computation.ExponentiationBySquaring import binary_partition, exp_by_squaring
+__all__=["long_division","long_multiplication","long_multiplication_steps",
+         "binary_partition", "exp_by_squaring"]
---
+++ Examples/ExponentiationBySquaringExample.py
@@ -0,0 +1,7 @@
+from Computation import binary_partition, exp_by_squaring
+
+print("The naieve method for calculating exponents is to multiply a number by itself n times. This requires n-1 multiplications. However there are much more efficient ways to calculate positive integer exponents.")
+
+print(binary_partition(471))
+
+print(exp_by_squaring(2,471))
