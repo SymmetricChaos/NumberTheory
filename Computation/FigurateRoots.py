e44766171441557236c64fd67bce1f956db9ff6b
SymmetricChaos <ajfraebel@gmail.com>
2019-05-30 19:38:46 -0400

    Update FigurateRoots.py

--- Computation/FigurateRoots.py
+++ Computation/FigurateRoots.py
@@ -1,15 +1,17 @@
 # The figurate numbers can be described by a quadratic equation
 
 # For example the triangular numbers
-# a = 0.5b(b+1)
+# x = 0.5b(b+1)
 # Can be rearranged as
-# a = 0.5b^2 + .5b
-# 2a = b^2 + b
-# 0 = b^2 + b - 2a
+# x = 0.5b^2 + .5b
+# 2x = b^2 + b
+# 0 = b^2 + b - 2x
 
 # Then the quadratic formula can give us the solution
+# (the coefficient are a = 1, b = 1, c = -2x)
 
-# b = ( -1 + sqrt(1-8a) )/2
+# Ignoring the negative solution
+# b = ( -1 + sqrt(1-8x) )/2
 
 from math import sqrt
 
