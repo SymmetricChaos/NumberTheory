2eac29bae1bd98325c0ef2cca0782beae7259b70
SymmetricChaos <ajfraebel@gmail.com>
2019-06-02 18:38:57 -0400

    Rising Factorial Example

---
+++ Examples/RisingFactorialExpansionExample.py
@@ -0,0 +1,8 @@
+from Polynomials.PolynomialType import Polynomial
+
+print("The rising factorial can be expressed as a series of polynomial.")
+
+R = Polynomial([0,1])
+for i in range(6):
+    print(R)
+    R = R * Polynomial([i+1,1])
--- Polynomials/RisingFactorialExpansion.py
+++
@@ -1,6 +0,0 @@
-from PolynomialType import Polynomial
-
-R = Polynomial([0,1])
-for i in range(6):
-    print(R)
-    R = R * Polynomial([i+1,1])
