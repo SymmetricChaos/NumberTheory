47a06f7a1db38658c229845da7fe440443a30e95
SymmetricChaos <ajfraebel@gmail.com>
2019-06-01 21:40:16 -0400

    Update SubsetSum.py
    commented out example

--- Other/SubsetSum.py
+++ Other/SubsetSum.py
@@ -53,28 +53,28 @@ def is_subset_sum(s,pos,n,out=[]):
     return is_subset_sum(s,pos-1,n,out) or is_subset_sum(s, pos-1, n-s[pos-1],out+[s[pos-1]])
 
 
-def subset_sum_dynamic(L,N):
-    """Faster solution to subset sum"""
-    L.sort(reverse=True)
-    if any([i < 0 for i in L]):
-        raise Exception("Doesn't work with negatives")
-    return is_subset_sum(L,len(L),N)
-    
-import random
-for i in range(5):
-    L = [random.randint(0,100) for i in range(25)]
-    N = random.randint(0,1000) 
-    print(N,L)
-    S = subset_sum_dynamic(L,N)
-    print(sum(S),S)
-    print("\n\n")
-
-#subset_sum_dynamic([-1,100],99)
-
-for i in range(5):
-    L = [random.randint(-100,100) for i in range(10)]
-    N = random.randint(-200,200) 
-    print(N,L)
-    for i in subset_sums(L,N):
-        print(sum(i),i)
-    print("\n\n")
+#def subset_sum_dynamic(L,N):
+#    """Faster solution to subset sum"""
+#    L.sort(reverse=True)
+#    if any([i < 0 for i in L]):
+#        raise Exception("Doesn't work with negatives")
+#    return is_subset_sum(L,len(L),N)
+#    
+#import random
+#for i in range(5):
+#    L = [random.randint(0,100) for i in range(25)]
+#    N = random.randint(0,1000) 
+#    print(N,L)
+#    S = subset_sum_dynamic(L,N)
+#    print(sum(S),S)
+#    print("\n\n")
+#
+##subset_sum_dynamic([-1,100],99)
+#
+#for i in range(5):
+#    L = [random.randint(-100,100) for i in range(10)]
+#    N = random.randint(-200,200) 
+#    print(N,L)
+#    for i in subset_sums(L,N):
+#        print(sum(i),i)
+#    print("\n\n")
