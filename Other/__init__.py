08cd709d6417ad62814bfb1946156b9065b440f1
SymmetricChaos <ajfraebel@gmail.com>
2019-06-02 06:09:49 -0400

    SubsetSumExample

--- Other/__init__.py
+++ Other/__init__.py
@@ -1,7 +1,7 @@
 from Other.Factorization import factorization, prime_factorization
 from Other.Hyperoperation import hyper
 from Other.Collatz import collatz
-from Other.SubsetSum import subset_sum
+from Other.SubsetSum import subset_sum, subset_sum_dynamic
 from Other.DigitalSum import digital_sum, digital_root, additive_persistence
 __all__=["factorization","prime_factorization","hyper","collatz","subset_sum",
-         "digital_sum","digital_root","additive_persistence"]
+         "digital_sum","digital_root","additive_persistence","subset_sum_dynamic"]
