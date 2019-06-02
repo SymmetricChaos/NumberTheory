21cf4a6f80a3e61faf45737e36de45d95227d57c
SymmetricChaos <ajfraebel@gmail.com>
2019-06-02 09:28:58 -0400

    BernoulliNumbers

--- Other/__init__.py
+++ Other/__init__.py
@@ -3,5 +3,7 @@ from Other.Hyperoperation import hyper
 from Other.Collatz import collatz
 from Other.SubsetSum import subset_sum, subset_sum_dynamic
 from Other.DigitalSum import digital_sum, digital_root, additive_persistence
+from Other.Choose import choose
 __all__=["factorization","prime_factorization","hyper","collatz","subset_sum",
-         "digital_sum","digital_root","additive_persistence","subset_sum_dynamic"]
+         "digital_sum","digital_root","additive_persistence","subset_sum_dynamic",
+         "choose"]
---
+++ Rationals/BernoulliNumbers.py
@@ -0,0 +1,17 @@
+from Other import choose
+from Rationals.RationalType import Rational
+
+def bernoulli_number(m):
+    if m > 2 and m % 2 == 1:
+        return Rational(0)
+    out = 0
+    for k in range(m+1):
+        for v in range(k+1):
+            
+            c = choose(k,v)
+            R = Rational(v**m,k+1)
+            out += R*c*(-1)**v
+    return out
+
+for i in range(50):
+    print(bernoulli_number(i))
