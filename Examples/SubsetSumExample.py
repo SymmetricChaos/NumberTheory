2241f306b55d7345420bc2820a8157f17c9147ec
SymmetricChaos <ajfraebel@gmail.com>
2019-06-02 06:18:34 -0400

    Update SubsetSumExample.py

--- Examples/SubsetSumExample.py
+++ Examples/SubsetSumExample.py
@@ -1,25 +1,24 @@
-def subset_sum_dynamic(L,N):
-    """Faster solution to subset sum"""
-    L.sort(reverse=True)
-    if any([i < 0 for i in L]):
-        raise Exception("Doesn't work with negatives")
-    return is_subset_sum(L,len(L),N)
-    
+from Other import subset_sum_dynamic
 import random
-for i in range(5):
+
+
+for i in range(3):
     L = [random.randint(0,100) for i in range(25)]
-    N = random.randint(0,1000) 
-    print(N,L)
+    N = random.randint(0,1200)
+    print("Given the set:")
+    print(L)
+    print(f"What is subset that has the sum {N}?")
     S = subset_sum_dynamic(L,N)
-    print(sum(S),S)
+    print(S)
     print("\n\n")
 
-#subset_sum_dynamic([-1,100],99)
 
-for i in range(5):
-    L = [random.randint(-100,100) for i in range(10)]
-    N = random.randint(-200,200) 
-    print(N,L)
-    for i in subset_sums(L,N):
-        print(sum(i),i)
-    print("\n\n")
+
+#
+#for i in range(5):
+#    L = [random.randint(-100,100) for i in range(10)]
+#    N = random.randint(-200,200) 
+#    print(N,L)
+#    for i in subset_sums(L,N):
+#        print(sum(i),i)
+#    print("\n\n")
