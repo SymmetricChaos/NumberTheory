84ce3d3d21ddaef826419747c87c671f5faab7eb
SymmetricChaos <ajfraebel@gmail.com>
2019-06-04 10:07:38 -0400

    Update RationalRoots.py
    comments

--- Polynomials/RationalRoots.py
+++ Polynomials/RationalRoots.py
@@ -9,23 +9,25 @@ def rational_roots(P):
     assert type(P) == Polynomial
     
     # Factor the leading and trailing coefficients
+    # We will include positive and negative factors for the numerator
     n = factorization(abs(P[0]))
     n = n + [-i for i in n]
     d = factorization(abs(P[-1]))
 
     R = []
 
+    # Test each of the possible roots
     for i in n:
         for j in d:
             q = Rational(i,j)
             r = P.evaluate(q)
-            if r[0] == Rational(0,1):
+            if r == Rational(0,1):
                 R.append(q)
     
     return R
 
                 
-P = Polynomial([-2,5,-5,3])
+P = Polynomial([6,-7,0,1])
 
 print(P)
 print(rational_roots(P))
