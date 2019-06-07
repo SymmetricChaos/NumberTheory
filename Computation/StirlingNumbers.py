e3b4fe141be2301e4318afce2cc941639650d569
SymmetricChaos <ajfraebel@gmail.com>
2019-06-06 20:29:23 -0400

    Update StirlingNumbers.py
    Stirling number of the first kind

--- Computation/StirlingNumbers.py
+++ Computation/StirlingNumbers.py
@@ -1,13 +1,34 @@
-# Stirling Numbers of the second kind
 
+# Stirling Numbers of the first kind
+def stirling_numbers_1(n,k):
+    if n == k:
+        return 1
+    if n == 0 and k == 0:
+        return 1
+    if n == 0 or k == 0:
+        return 0
+    return (n-1)*stirling_numbers_1(n-1,k) + stirling_numbers_1(n-1,k-1)
+
+
+
+# Stirling Numbers of the second kind
 def stirling_numbers_2(n,k):
+    if n == k:
+        return 1
     if n == 0 and k == 0:
         return 1
     if n == 0 or k == 0:
         return 0
     return k*stirling_numbers_2(n-1,k) + stirling_numbers_2(n-1,k-1)
 
-for i in range(10):
-    for j in range(i):
-        print(stirling_numbers_2(i,j),end=" ")
-    print()
+
+
+#for i in range(9):
+#    for j in range(i+1):
+#        print(stirling_numbers_1(i,j),end=" ")
+#    print()
+#print()
+#for i in range(9):
+#    for j in range(i+1):
+#        print(stirling_numbers_2(i,j),end=" ")
+#    print()
