53ab680e01142d77dfac4a610e1e26cbb69fe6cc
SymmetricChaos <ajfraebel@gmail.com>
2019-06-07 19:48:03 -0400

    Stirling Numbers importable

--- Computation/StirlingNumbers.py
+++ Computation/StirlingNumbers.py
@@ -1,6 +1,6 @@
 
-# Stirling Numbers of the first kind
 def stirling_numbers_1(n,k):
+    """Stirling Numbers of the First Kind"""
     if n == k:
         return 1
     if n == 0 and k == 0:
@@ -11,8 +11,8 @@ def stirling_numbers_1(n,k):
 
 
 
-# Stirling Numbers of the second kind
 def stirling_numbers_2(n,k):
+    """Stirling Numbers of the Second Kind"""
     if n == k:
         return 1
     if n == 0 and k == 0:
@@ -22,13 +22,3 @@ def stirling_numbers_2(n,k):
     return k*stirling_numbers_2(n-1,k) + stirling_numbers_2(n-1,k-1)
 
 
-
-#for i in range(9):
-#    for j in range(i+1):
-#        print(stirling_numbers_1(i,j),end=" ")
-#    print()
-#print()
-#for i in range(9):
-#    for j in range(i+1):
-#        print(stirling_numbers_2(i,j),end=" ")
-#    print()
--- Computation/__init__.py
+++ Computation/__init__.py
@@ -2,5 +2,7 @@ from Computation.LongDivision import long_division
 from Computation.LongMultiplication import long_multiplication, long_multiplication_steps
 from Computation.ExponentiationBySquaring import binary_partition, exp_by_squaring
 from Computation.AdditionChain import addition_chain
+from Computation.StirlingNumbers import stirling_numbers_1, stirling_numbers_2
 __all__=["long_division","long_multiplication","long_multiplication_steps",
-         "binary_partition", "exp_by_squaring", "addition_chain"]
+         "binary_partition", "exp_by_squaring", "addition_chain",
+         "stirling_numbers_1", "stirling_numbers_2"]
