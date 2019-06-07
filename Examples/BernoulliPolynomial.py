d9e442e7fedf9d30ff2050abaf53d7e8293daf50
SymmetricChaos <ajfraebel@gmail.com>
2019-06-07 19:44:12 -0400

    minor chance

--- Examples/BernoulliPolynomial.py
+++ Examples/BernoulliPolynomial.py
@@ -2,7 +2,7 @@ from Rationals import bernoulli_number, Rational
 from Polynomials import Polynomial
 from Other import choose
 
-for n in range(1,7):
+for n in range(1,9):
     P = Polynomial([0])
     for k in range(n+1):
         b = bernoulli_number(n-k)
