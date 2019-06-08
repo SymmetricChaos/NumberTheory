5477e266c52beffc866bdbb434ea50c3aa42a5b7
SymmetricChaos <ajfraebel@gmail.com>
2019-06-08 09:28:32 -0400

    Removed Other
    Moved most stuff to computation which has a somewhat clearer purpose

--- Combinatorics/__init__.py
+++ Combinatorics/__init__.py
@@ -1,2 +1,4 @@
 from Combinatorics.StirlingNumbers import stirling_numbers_1, stirling_numbers_2
-__all__=["stirling_numbers_1", "stirling_numbers_2"]
+from Combinatorics.Choose import choose
+from Combinatorics.Combinations import combo_pairs
+__all__=["stirling_numbers_1", "stirling_numbers_2", "choose", "combo_pairs"]
--- Other/__init__.py
+++
@@ -1,9 +0,0 @@
-from Other.Factorization import factorization, prime_factorization
-from Other.Hyperoperation import hyper
-from Other.Collatz import collatz
-from Other.SubsetSum import subset_sum, subset_sum_dynamic
-from Other.DigitalSum import digital_sum, digital_root, additive_persistence
-from Other.Choose import choose
-__all__=["factorization","prime_factorization","hyper","collatz","subset_sum",
-         "digital_sum","digital_root","additive_persistence","subset_sum_dynamic",
-         "choose"]
