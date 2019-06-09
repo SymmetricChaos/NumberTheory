76628625ac90f2c8fa7b324e64fd3c588a9471a1
SymmetricChaos <ajfraebel@gmail.com>
2019-06-09 12:40:02 -0400

    Create FourierVisualization.py

---
+++ Visualization/FourierVisualization.py
@@ -0,0 +1,17 @@
+from Fourier import Fourier, evaluate_series
+import numpy as np
+
+A = Fourier([1],[1],[1])
+B = Fourier([1],[2],[2])
+C = A+B
+x = np.linspace(-5,5,100)
+
+yA = evaluate_series(A,x)
+yB = evaluate_series(B,x)
+yC = evaluate_series(C,x)
+
+import matplotlib.pyplot as plt
+
+plt.plot(x,yA,color='grey',linestyle=":")
+plt.plot(x,yB,color='grey',linestyle=":")
+plt.plot(x,yC,color='black')
