f3bcb1cf2ef7561b79461ac49641bc929521d07f
SymmetricChaos <ajfraebel@gmail.com>
2019-06-09 07:19:53 -0400

    Update LagrangeInterpolationVisualization.py

--- Visualization/LagrangeInterpolationVisualization.py
+++ Visualization/LagrangeInterpolationVisualization.py
@@ -2,20 +2,27 @@ from Polynomials import lagrange_interpolation
 import matplotlib.pyplot as plt
 import numpy as np
 
+points = np.array([1,3,5,7])
+function = lambda x: np.sin(x)
+
+
+
+print("""Lagrange interpolation takes a set of n points and finds the "best" polynomial that describes them. Given n points on a plane there is a polynomial of degree n-1 that passes through all of them.""")
+print(f"In this example we use {len(points)} points taken from the sine function.")
+
+
 fig = plt.figure()
 ax=fig.add_axes([0,0,1,1])
 
-F = lambda x: x**x
-lp = lagrange_interpolation([1,2,3,4],F)
+lp = lagrange_interpolation(points,function)
 
 print(lp)
 
-x0 = np.linspace(1,4,20)
-y0 = F(x0)
+x = np.linspace(min(points),max(points),50)
 
+y0 = function(x)
+y1 = lp.evaluate(x)
 
-x1 = np.linspace(1,4,20)
-y1 = lp.evaluate(x1)
-plt.plot(x1,y1)
-plt.plot(x0,y0)
-plt.scatter([1,2,3,4],[F(1),F(2),F(3),F(4)])
+plt.plot(x,y0)
+plt.plot(x,y1)
+plt.scatter(points,function(points))
