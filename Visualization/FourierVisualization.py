fc6aa9b6a5c5ccf0de356afb3c2a3307c06d2893
SymmetricChaos <ajfraebel@gmail.com>
2019-06-09 13:59:31 -0400

    Update FourierVisualization.py
    Better example

--- Visualization/FourierVisualization.py
+++ Visualization/FourierVisualization.py
@@ -1,19 +1,22 @@
 from Fourier import Fourier, evaluate_series
 import numpy as np
 
-A = Fourier([1],[1],[1])
+A = Fourier([2],[1],[1])
 B = Fourier([1.5],[3],[2])
-C = A+B
-x = np.linspace(-5,5,150)
+C = Fourier([1],[.5],[1.2])
+S = A+B+C
+x = np.linspace(-8,8,150)
 
 yA = evaluate_series(A,x)
 yB = evaluate_series(B,x)
 yC = evaluate_series(C,x)
+yS = evaluate_series(S,x)
 
 import matplotlib.pyplot as plt
 
 plt.plot(x,yA,color='grey',linestyle=":")
 plt.plot(x,yB,color='grey',linestyle=":")
-plt.plot(x,yC,color='black')
-print(A)
-print(B)
+plt.plot(x,yC,color='grey',linestyle=":")
+plt.plot(x,yS,color='black')
+
+print(D)
