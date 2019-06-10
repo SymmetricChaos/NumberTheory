8d51159ea0c0f9efaa3d356371df15b678e38a5d
SymmetricChaos <ajfraebel@gmail.com>
2019-06-10 18:29:15 -0400

    Fixed Imports

--- Combinatorics/LobbNumbers.py
+++ Combinatorics/LobbNumbers.py
@@ -1,4 +1,4 @@
-from Choose import choose
+from Combinatorics.Choose import choose
 
 def lobb_number(m,n):
     assert type(m) == int
@@ -8,8 +8,3 @@ def lobb_number(m,n):
     b = 2*m+1
     c = m+n+1
     return (a*b)//c
-#
-#for i in range(6):
-#    for j in range(i+1):
-#        print(lobb_number(j,i),end = " ")
-#    print()
--- Combinatorics/__init__.py
+++ Combinatorics/__init__.py
@@ -3,5 +3,6 @@ from Combinatorics.Choose import choose
 from Combinatorics.Combinations import combo_pairs
 from Combinatorics.Factorial import factorial
 from Combinatorics.RisingAndFallingFactorial import rising_factorial, falling_factorial
+from Combinatorics.LobbNumbers import lobb_number
 __all__=["stirling_numbers_1", "stirling_numbers_2", "choose", "combo_pairs",
-         "factorial", "rising_factorial", "falling_factorial"]
+         "factorial", "rising_factorial", "falling_factorial", "lobb_number"]
