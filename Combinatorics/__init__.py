fa2728c3a1c81d7effaf20e29f695090fd9c5d46
SymmetricChaos <ajfraebel@gmail.com>
2019-06-12 06:59:48 -0400

    Update __init__.py

--- Combinatorics/__init__.py
+++ Combinatorics/__init__.py
@@ -5,7 +5,7 @@ from Combinatorics.Factorial import factorial
 from Combinatorics.RisingAndFallingFactorial import rising_factorial, falling_factorial
 from Combinatorics.LobbNumbers import lobb_number
 from Combinatorics.DyckWords import dyck_words
-
+from Combinatorics.NarayanaNumbers import narayana_number
 __all__=["stirling_numbers_1", "stirling_numbers_2", "choose", "combo_pairs",
          "factorial", "rising_factorial", "falling_factorial", "lobb_number",
-         "dyck_words"]
+         "dyck_words", "narayana_number"]
