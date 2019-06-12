6da5675c0733f268643ddf9729a3718b4e3b20b4
SymmetricChaos <ajfraebel@gmail.com>
2019-06-12 19:18:38 -0400

    Update StirlingNumbersExample.py

--- Examples/StirlingNumbersExample.py
+++ Examples/StirlingNumbersExample.py
@@ -1,12 +1,13 @@
 from Combinatorics import stirling_numbers_1, stirling_numbers_2
 from GeneralUtils import equal_spacing
 
+print("Stirling Numbers of the First Kind")
 for i in range(8):
     L = []
     for j in range(i+1):
         L.append(stirling_numbers_1(i,j))
     equal_spacing(L,5,'left')
-print()
+print("\nStirling Numbers of the Second Kind")
 for i in range(8):
     L = []
     for j in range(i+1):
