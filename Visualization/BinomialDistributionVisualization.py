d10f8a559da282ee849cf58a9cf12d4571ffb28d
SymmetricChaos <ajfraebel@gmail.com>
2019-06-13 17:49:43 -0400

    Update BinomialDistributionVisualization.py

--- Visualization/BinomialDistributionVisualization.py
+++ Visualization/BinomialDistributionVisualization.py
@@ -2,13 +2,19 @@ from Combinatorics.Distributions import BinomialDist
 import matplotlib.pyplot as plt
 
 
-D1 = BinomialDist(20,.7)
+D1 = BinomialDist(20,.5)
 x1 = [i for i in range(21)]
 y1 = [i for i in D1]
 
-D2 = BinomialDist(14,.4)
-x2 = [i for i in range(15)]
+D2 = BinomialDist(20,.7)
+x2 = [i for i in range(21)]
 y2 = [i for i in D2]
 
-plt.scatter(x1,y1)
-plt.scatter(x2,y2)
+D3 = BinomialDist(40,.5)
+x3 = [i for i in range(41)]
+y3 = [i for i in D3]
+
+
+plt.plot(x1,y1)
+plt.plot(x2,y2)
+plt.plot(x3,y3)
