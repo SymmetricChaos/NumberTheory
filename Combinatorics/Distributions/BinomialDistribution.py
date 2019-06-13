dc9eba467fa9337a1de5e1354b79bf9cea649f01
SymmetricChaos <ajfraebel@gmail.com>
2019-06-13 08:50:48 -0400

    Update BinomialDistribution.py
    As a class

--- Combinatorics/Distributions/BinomialDistribution.py
+++ Combinatorics/Distributions/BinomialDistribution.py
@@ -1,9 +1,25 @@
 from Combinatorics import choose
 
-def binomial_distribution(n,p):
-    pmf = []
-    for k in range(n+1):
-        pmf.append( choose(n,k)*p**k*(1-p)**(n-k) )
-    return pmf
+class BinomialDist:
+    
+    def __init__(self,n,p):
+        self.n = n
+        self.p = p
+        pmf = []
+        for k in range(n+1):
+            pmf.append( choose(n,k)*p**k*(1-p)**(n-k) )
+        self.pmf = pmf
+        self.mean = n*p
+        
+        
+    def  __getitem__(self,k):
+        return self.pmf[k]
 
-#print(binomial_distribution(20,.7))
+    def __iter__(self):
+        for i in self.pmf:
+            yield i
+            
+#D = BinomialDist(20,.7)
+#
+#for i in D:
+#    print(i)
