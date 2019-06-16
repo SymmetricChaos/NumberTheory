41eaa19073e9e84af13c6f5ed0fc951df5ba12de
SymmetricChaos <ajfraebel@gmail.com>
2019-06-16 08:23:42 -0400

    Update __init__.py
    Reorganized to make sure nothing is missed

--- Combinatorics/__init__.py
+++ Combinatorics/__init__.py
@@ -1,12 +1,20 @@
-from Combinatorics.StirlingNumbers import stirling_numbers_1, stirling_numbers_2
+from Combinatorics.CatalanNumbers import catalan
 from Combinatorics.Choose import choose
 from Combinatorics.Combinations import combo_pairs
-from Combinatorics.Factorial import factorial
-from Combinatorics.RisingAndFallingFactorial import rising_factorial, falling_factorial
-from Combinatorics.LobbNumbers import lobb_number
 from Combinatorics.DyckWords import dyck_words
+from Combinatorics.Factorial import factorial
+from Combinatorics.LobbNumbers import lobb_number
 from Combinatorics.NarayanaNumbers import narayana_number
-from Combinatorics.CatalanNumbers import catalan
-__all__=["stirling_numbers_1", "stirling_numbers_2", "choose", "combo_pairs",
-         "factorial", "rising_factorial", "falling_factorial", "lobb_number",
-         "dyck_words", "narayana_number","catalan"]
+from Combinatorics.RisingAndFallingFactorial import rising_factorial, falling_factorial
+from Combinatorics.StirlingNumbers import stirling_numbers_1, stirling_numbers_2
+
+__all__=["catalan",
+         "choose",
+         "combo_pairs",
+         "dyck_words",
+         "factorial",
+         "lobb_number",
+         "narayana_number",
+         "rising_factorial", "falling_factorial", 
+         "stirling_numbers_1", "stirling_numbers_2",
+          ]
