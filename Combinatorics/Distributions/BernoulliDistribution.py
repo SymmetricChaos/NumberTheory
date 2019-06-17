ec8c56b9cf848a49b1518a61ab1f977b560bbec8
SymmetricChaos <ajfraebel@gmail.com>
2019-06-17 07:28:47 -0400

    Create BernoulliDistribution.py

---
+++ Combinatorics/Distributions/BernoulliDistribution.py
@@ -0,0 +1,23 @@
+def bernoulli_eq(p,k):
+     if k == 0:
+         return 1-p
+     if k == 1:
+         return p
+
+
+class BernoulliDist:
+    
+    def __init__(self,p):
+        self.n = 2
+        self.p = p
+        self.q = 1-p
+        self.mean = p
+        
+        
+    def  __getitem__(self,k):
+        return bernoulli_eq(self.p,k)
+    
+    def __iter__(self):
+        for k in range(self.n+1):
+            yield bernoulli_eq(self.p,k)
+
