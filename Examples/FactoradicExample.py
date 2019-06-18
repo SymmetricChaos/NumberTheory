8d3e1f759216339722db02ce4b9d64ed0bcc0baa
SymmetricChaos <ajfraebel@gmail.com>
2019-06-18 19:25:18 -0400

    Update FactoradicExample.py

--- Examples/FactoradicExample.py
+++ Examples/FactoradicExample.py
@@ -1,3 +1,7 @@
 from Numerals import factoradic
 
-print(factoradic(7456))
+print("The factoradic numbers are an unusual way of represent numbers. Usuaully numeral systems have place values that increase by powers of some integer. Factoradic numbers have place values that are successive factorials.")
+
+print(factoradic(74562))
+
+print("Besides being an interesting concept factoradics are useful in working with permutations.")
