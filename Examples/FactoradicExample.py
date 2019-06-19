e7d528cb4788ad3c533d3c3cabf3136454b3c90f
SymmetricChaos <ajfraebel@gmail.com>
2019-06-19 19:28:04 -0400

    Update FactoradicExample.py

--- Examples/FactoradicExample.py
+++ Examples/FactoradicExample.py
@@ -2,6 +2,30 @@ from Numerals import factoradic
 
 print("The factoradic numbers are an unusual way of represent numbers. Usuaully numeral systems have place values that increase by powers of some integer. Factoradic numbers have place values that are successive factorials.")
 
-print(factoradic(74562))
+print("\nWe can write 67 as:")
 
-print("Besides being an interesting concept factoradics are useful in working with permutations.")
+print(factoradic(67))
+
+print("Because:")
+
+print("4! * 2 + 3! * 3 + 2! * 0 + 1! * 1 + 0! * 0")
+print("24*2 + 6*3 + 2*0 + 1*1 + 1*0")
+print("48 + 18 + 0 + 1 + 0")
+print("67")
+
+print("\nBesides being an interesting concept factoradics are useful in working with permutations.")
+
+def factoradic_permutation(L,f):
+    
+    F = factoradic(f)
+    out = []
+    for i in F:
+        out.append( L.pop(i) ) 
+    while len(L) > 0:
+        out.append( L.pop(0) )
+    return out
+    
+
+for i in range(20):
+    L = ["A","B","C","D"]
+    print("".join(factoradic_permutation(L,i)))
