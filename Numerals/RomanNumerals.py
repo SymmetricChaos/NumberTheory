5127b3d4b95ee6ee613c0a0ff1304e0cd9855de7
SymmetricChaos <ajfraebel@gmail.com>
2019-06-20 17:04:53 -0400

    Update RomanNumerals.py
    comments

--- Numerals/RomanNumerals.py
+++ Numerals/RomanNumerals.py
@@ -9,19 +9,21 @@ def roman_to_int(n):
     for r in n:
         L.append(Roman[r])
     
-    # Step
+    # Step through the numerals adding or subtracting as needed
     for i in range(len(L)-1):
         a, b = L[i],L[i+1]
         if a >= b:
             out += a
         else:
             out -= a
+    
+    # The final Roman numeral is always added
     out += L[-1]
             
     return out
-    
-    
-#
+
+
 #def int_to_roman(n):
-        
+    
+
 print(roman_to_int("MCMLIV"))
