01558384e7b22512e91df1ac8da66ac3c5d3cbf8
SymmetricChaos <ajfraebel@gmail.com>
2019-06-20 16:38:11 -0400

    Create RomanNumerals.py

---
+++ Numerals/RomanNumerals.py
@@ -0,0 +1,27 @@
+Roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+
+
+def roman_to_int(n):
+    out = 0
+    L = []
+    
+    # Convert the Roman numerals to integer values
+    for r in n:
+        L.append(Roman[r])
+    
+    # Step
+    for i in range(len(L)-1):
+        a, b = L[i],L[i+1]
+        if a >= b:
+            out += a
+        else:
+            out -= a
+    out += L[-1]
+            
+    return out
+    
+    
+#
+#def int_to_roman(n):
+        
+print(roman_to_int("MCMLIV"))
