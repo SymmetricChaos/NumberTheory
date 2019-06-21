93fb1e7b3b2a8665c170a79a64912dfd0c778e1a
SymmetricChaos <ajfraebel@gmail.com>
2019-06-21 07:43:29 -0400

    Update RomanNumerals.py

--- Numerals/RomanNumerals.py
+++ Numerals/RomanNumerals.py
@@ -1,5 +1,9 @@
 Roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
 
+# Ones, tens, and hundred digits
+R1   = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+R10  = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+R100 = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
 
 def roman_to_int(n):
     out = 0
@@ -23,7 +27,19 @@ def roman_to_int(n):
     return out
 
 
-#def int_to_roman(n):
+def int_to_roman(n):
+    
+    d = [int(i) for i in str(n%1000)]
+    
+    L = ["M"]*(n//1000)
+    
+    L.append( R100[d[0]] )
+    L.append( R10[d[1]] )
+    L.append( R1[d[2]] )
+    
+    return "".join(L)
     
 
 print(roman_to_int("MCMLIV"))
+
+print(int_to_roman(1954))
