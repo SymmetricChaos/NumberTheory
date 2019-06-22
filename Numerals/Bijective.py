2da4892103344fa022984c67db2df5b076d67945
SymmetricChaos <ajfraebel@gmail.com>
2019-06-22 11:14:50 -0400

    Update Bijective.py
    Added class

--- Numerals/Bijective.py
+++ Numerals/Bijective.py
@@ -19,7 +19,35 @@ def int_to_bijective(n,b):
         q0 = q1
     return "".join(reversed(S))
         
+class BijectiveBase:
     
-print(bijective_to_int("3122",3))
-print(int_to_bijective(98,3))
-#class BijectiveBase
+    def __init__(self,n,b=2):
+        self.n = n
+        self.b = b
+    
+    def __str__(self):
+        return int_to_bijective(self.n,self.b)
+    
+    def __repr__(self):
+        return int_to_bijective(self.n,self.b)
+    
+    def __add__(self,addend):
+        assert self.b == addend.b
+        return BijectiveBase(self.n+addend.n,self.b)
+    
+    def __mul__(self,addend):
+        assert self.b == addend.b
+        return BijectiveBase(self.n*addend.n,self.b)
+    
+    def __int__(self):
+        return self.n
+    
+A = BijectiveBase(4354,5)
+B = BijectiveBase(1726,5)
+print(A)
+print(B)
+print(A+B)
+
+
+#print(bijective_to_int("3122",3))
+#print(int_to_bijective(98,3))
