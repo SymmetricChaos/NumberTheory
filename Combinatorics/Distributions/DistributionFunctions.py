af2f4317d668b8f0e27b36e3ae6dbb814ed5f871
SymmetricChaos <ajfraebel@gmail.com>
2019-06-23 17:13:42 -0400

    Update DistributionFunctions.py
    Function to check of a number is a valid probability

--- Combinatorics/Distributions/DistributionFunctions.py
+++ Combinatorics/Distributions/DistributionFunctions.py
@@ -2,3 +2,8 @@ from math import sqrt
 
 def std_dev(distribution):
     return sqrt(distribution.var)
+
+def is_prob(p):
+    assert p > 0, "probability must be greater than zero"
+    assert p < 1, "probability must be less than one"
+    return True
