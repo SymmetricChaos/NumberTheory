a779b5271f9d32a7555364fc9c126f3ccd7229d0
SymmetricChaos <ajfraebel@gmail.com>
2019-06-23 17:12:34 -0400

    Renamed and updated

--- Combinatorics/Distributions/UniformDistribution.py
+++ Combinatorics/Distributions/UniformDistribution.py
@@ -1,24 +1,24 @@
-def discrete_eq(a,b,k,v):
+def uniform_eq(a,b,k,v):
     if k >= a and k <= b:
         if type(k) == int:
             return v
     return 0
 
 
-class DiscreteDist:
+class UniformDist:
     
     def __init__(self,a,b):
         assert a < b
+        assert type(a) == int
+        assert type(b) == int
         self.a = a
         self.b = b
         self.n = b-a+1
         self.mean = (a+b)/2
         self.var = ((b-a+1)**2 - 1)/12
+        self.support = range(a,b+1)
         
         
     def  __getitem__(self,k):
-        return discrete_eq(self.a,self.b,k,1/self.n)
-    
-    def __iter__(self):
-        for k in range(self.a,self.b+1):
-            yield discrete_eq(self.a,self.b,k,1/self.n)
+        assert k in self.support
+        return uniform_eq(self.a,self.b,k,1/self.n)
