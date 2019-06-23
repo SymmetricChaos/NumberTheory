6ecf30138a86430b2c01466abd44ec56192eb78c
SymmetricChaos <ajfraebel@gmail.com>
2019-06-23 13:16:06 -0400

    Update GeometricDistribution.py

--- Combinatorics/Distributions/GeometricDistribution.py
+++ Combinatorics/Distributions/GeometricDistribution.py
@@ -1,12 +1,13 @@
 def geometric_eq(p,k):
-     return (1-p)**(k-1)*p
+     return (1-p)**k*p
 
 
 class GeometricDist:
     
     def __init__(self,p):
         self.p = p
-        self.mean = 1/p
+        self.mean = (1-p)/p
+        self.var = (1-p)/p**2
         
         
     def  __getitem__(self,k):
