bfac9fd427ba2638bde892b330ce0e6aa726eb79
SymmetricChaos <ajfraebel@gmail.com>
2019-06-23 17:14:01 -0400

    Update __init__.py
    Added uniform distribution

--- Combinatorics/Distributions/__init__.py
+++ Combinatorics/Distributions/__init__.py
@@ -1,6 +1,9 @@
 from Combinatorics.Distributions.BinomialDistribution import BinomialDist
 from Combinatorics.Distributions.PoissonDistribution import PoissonDist
 from Combinatorics.Distributions.GeometricDistribution import GeometricDist
+from Combinatorics.Distributions.UniformDistribution import UniformDist
 
 
-__all__=["BinomialDist","PoissonDist","GeometricDist"]
+from Combinatorics.Distributions.DistributionFunctions import std_dev
+
+__all__=["BinomialDist","PoissonDist","GeometricDist","UniformDist","std_dev"]
