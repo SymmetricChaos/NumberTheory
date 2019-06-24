32ca1d05ed034e40646f021afa7d74321c33f7da
SymmetricChaos <ajfraebel@gmail.com>
2019-06-24 17:14:11 -0400

    Update EloSystem.py
    Commented out example

--- Elo/EloSystem.py
+++ Elo/EloSystem.py
@@ -14,17 +14,19 @@ def elo_update(A,B,scoreA,scoreB,K=32):
     
     return A,B
 
-p1 = 1613
-p2 = 1609
-p3 = 1477
-p4 = 1388
-p5 = 1586
-p6 = 1720
-print(p1)
-p1,p2 = elo_update(p1,p2,0,1)
-p1,p3 = elo_update(p1,p3,.5,.5)
-p1,p4 = elo_update(p1,p4,1,0)
-p1,p5 = elo_update(p1,p5,1,0)
-p1,p6 = elo_update(p1,p6,0,1)
-
-print(p1)
+#p1 = 1613
+#p2 = 1609
+#p3 = 1477
+#p4 = 1388
+#p5 = 1586
+#p6 = 1720
+#
+#print(p1)
+#
+#p1,p2 = elo_update(p1,p2,1,1)
+#p1,p3 = elo_update(p1,p3,.5,.5)
+#p1,p4 = elo_update(p1,p4,1,0)
+#p1,p5 = elo_update(p1,p5,1,0)
+#p1,p6 = elo_update(p1,p6,0,1)
+#
+#print(p1)
