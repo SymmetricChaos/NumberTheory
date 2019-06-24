897e6758ccf484513d566707e2b3fce378c241ec
SymmetricChaos <ajfraebel@gmail.com>
2019-06-24 17:13:57 -0400

    Update DictManip.py

--- GeneralUtils/DictManip.py
+++ GeneralUtils/DictManip.py
@@ -1,9 +1,7 @@
-import operator
-
 def show_dict(D):
     for i,j in D.items():
         print(f"{i}: {j}")
         
 def sort_by_values(D):
-    L = sorted(D.items(), key=operator.itemgetter(1))
+    L = sorted(D.items(), key=lambda kv: kv[1])
     return L
