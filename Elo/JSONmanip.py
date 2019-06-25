f65763b0fe1d1e81ca7dededb2d90e164d9a5fd9
SymmetricChaos <ajfraebel@gmail.com>
2019-06-25 19:49:52 -0400

    Renamed

--- Elo/JSONmanip.py
+++ Elo/JSONmanip.py
@@ -1,9 +1,3 @@
-import json
-from Tournament import Tournament
-
-with open("OWLmatches.json", "r") as read_file:
-    data = json.load(read_file)
-
 
 def match_score(D):
     A = D["competitors"][0]["abbreviatedName"]
@@ -37,27 +31,3 @@ def stage_playoff(L,n):
             if i[4] == n:
                 out.append(i)
     return out
-
-S = match_scores(data)
-stage_1 = stage_regular(S,1)
-stage_2 = stage_regular(S,2)
-
-
-players = ["PHI","LDN","NYE","BOS","SEO","GLA","SHD","HZS",
-           "TOR","HOU","ATL","FLA","DAL","SFS","CDH","GZC",
-           "PAR","WAS","VAL","VAN"]
-
-T = Tournament(players,start_score=1000,K=32)
-
-for i in stage_1:
-    T.update(i[0],i[1],i[2],i[3])
-    
-print(T)
-
-T = Tournament(players,start_score=1000,K=32)
-
-for i in stage_2:
-    T.update(i[0],i[1],i[2],i[3])
-    
-print(T)
-    
