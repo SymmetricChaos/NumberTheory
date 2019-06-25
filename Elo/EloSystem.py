839a193098d7eb4e4e2608c0541ed34a61d8503b
SymmetricChaos <ajfraebel@gmail.com>
2019-06-25 18:00:33 -0400

    Update EloSystem.py
    Alternative computation

--- Elo/EloSystem.py
+++ Elo/EloSystem.py
@@ -1,10 +1,17 @@
 from math import ceil
 
-def elo_expected(A,B):
-    expectedA = 1/(1+10**((B-A)/400))
-    expectedB = 1/(1+10**((A-B)/400))
-    return expectedA, expectedB
+#def elo_expected(A,B):
+#    expectedA = 1/(1+10**((B-A)/400))
+#    expectedB = 1/(1+10**((A-B)/400))
+#    return expectedA, expectedB
 
+
+def elo_expected(A,B,n=100):
+    x = A-B
+    ex = -(x/n)
+    expectedA = 1/(1+10**ex)
+    expectedB = 1 - expectedA
+    return expectedA, expectedB
 
 
 def elo_update(A,B,scoreA,scoreB,K=32):
