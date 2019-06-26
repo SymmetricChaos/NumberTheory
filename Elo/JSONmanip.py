75ecb9582520904ead860e32b67b62df66322d21
SymmetricChaos <ajfraebel@gmail.com>
2019-06-25 20:57:54 -0400

    Update JSONmanip.py
    Exclude incomplete matches
    Also there are three reasonable ways to score matches: map wins, points taken, and win/loss. Will add win/loss next
    And better names.

--- Elo/JSONmanip.py
+++ Elo/JSONmanip.py
@@ -1,19 +1,35 @@
 
 def match_score(D):
+    a = D["scores"][0]["value"]
+    b = D["scores"][1]["value"]
+    return a,b
+
+def map_score(D):
+    a = 0
+    b = 0
+    for i in D["games"]:
+        a += i["points"][0]
+        b += i["points"][1]
+    return a,b
+
+def match_values(D):
     A = D["competitors"][0]["abbreviatedName"]
     B = D["competitors"][1]["abbreviatedName"]
-    As = D["scores"][0]["value"]
+    As,Bs = match_score(D)
     Bs = D["scores"][1]["value"]
     stg = D["bracket"]["stage"]["tournament"]["attributes"]["program"]["stage"]["number"]
     frm = D["bracket"]["stage"]["tournament"]["attributes"]["program"]["stage"]["format"]
     return A,B,As,Bs,stg, frm
 
-def match_scores(js):
+
+def all_matches(js,score_type="matches"):
     out = []
     N = js["totalElements"]
+    
     for i in range(N):
         D = js["content"][i]
-        out.append( match_score(D) )
+        if D["status"] == "CONCLUDED":
+            out.append( match_values(D) )
     return out
 
 def stage_regular(L,n):
