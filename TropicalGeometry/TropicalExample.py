8723ca3dbf740430261915997afd3b01fdd5f381
SymmetricChaos <ajfraebel@gmail.com>
2019-06-30 08:42:45 -0400

    Update TropicalExample.py

--- TropicalGeometry/TropicalExample.py
+++ TropicalGeometry/TropicalExample.py
@@ -1,10 +1,19 @@
-from TropicalSemiring import Tropical
+from TropicalSemiringQ import Tropical
 from Polynomials import Polynomial
 
-a = Tropical(1)
-b = Tropical(2)
-c = Tropical(3)
+#a = Tropical(1)
+#b = Tropical(2)
+#c = Tropical(3)
+#
+#T = Polynomial([a,b,c])
+#
+#print(T)
 
-T = Polynomial([a,b,c])
+a = Tropical(5,7)
+b = Tropical(8,5)
 
-print(a**0)
+print(a)
+print(b)
+print(a+b)
+print(a*b)
+print(a**b)
