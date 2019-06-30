1d2d29dde17d7f4979fca0c0456d998d598ff954
SymmetricChaos <ajfraebel@gmail.com>
2019-06-30 11:50:01 -0400

    Update TropicalExample.py
    More interesting polynomial

--- TropicalGeometry/TropicalExample.py
+++ TropicalGeometry/TropicalExample.py
@@ -1,6 +1,7 @@
 from TropicalSemiring import Tropical
 from TropicalPolynomial import TropicalPoly
 import numpy as np
+import matplotlib.pyplot as plt
 
 a = Tropical(5)
 b = Tropical(8)
@@ -12,15 +13,16 @@ print(a*b)
 print(a**b)
 
 
-P = TropicalPoly([b,a])
+
+a = Tropical(3)
+b = Tropical(1)
+c = Tropical(1)
+P = TropicalPoly([a,b,c])
 print(P)
 
 x = list(np.linspace(-2,6,51))
 y = P.evaluate(x)
 
 
-import matplotlib.pyplot as plt
 
 plt.plot(x,y)
-
-print(a**0)
