895af092ece88ea29837d3dee2224f36ada86d63
SymmetricChaos <ajfraebel@gmail.com>
2019-06-30 11:46:14 -0400

    Update TropicalSemiring.py
    More possible operations

--- TropicalGeometry/TropicalSemiring.py
+++ TropicalGeometry/TropicalSemiring.py
@@ -3,12 +3,50 @@ class Tropical:
     def __init__(self,val):
         self.val = val
     
+    # Relations
+    def __lt__(self,other):
+        if type(other) == Tropical:
+            return self.val-other.val < 0
+        else:
+            return self.val-other < 0
+    
+    def __gt__(self,other):
+        if type(other) == Tropical:
+            return self.val-other.val > 0
+        else:
+            return self.val-other > 0
+    
+    def __le__(self,other):
+        if type(other) == Tropical:
+            return self.val-other.val <= 0
+        else:
+            return self.val-other <= 0
+    
+    def __ge__(self,other):
+        if type(other) == Tropical:
+            return self.val-other.val >= 0
+        else:
+            return self.val-other >= 0
+    
+    def __eq__(self,other):
+        if type(other) == Tropical:
+            return self.val == other.val
+        else:
+            return self.val == other
+    
+    
+    # Simple operations
     def __add__(self,b):
         if type(b) == Tropical:
             return Tropical(min(self.val,b.val))
         else:
             return Tropical(min(self.val,b))
-    
+        
+    def __radd__(self,b):
+        if type(b) == Tropical:
+            return Tropical(min(self.val,b.val))
+        else:
+            return Tropical(min(self.val,b))
     
     def __mul__(self,b):
         if type(b) == Tropical:
@@ -16,17 +54,42 @@ class Tropical:
         else:
             return Tropical(self.val+b)
         
-    
+    def __rmul__(self,b):
+        if type(b) == Tropical:
+            return Tropical(self.val+b.val)
+        else:
+            return Tropical(self.val+b)
+        
     def __pow__(self,b):
         if type(b) == Tropical:
             return Tropical(self.val*b.val)
         else:
             return Tropical(self.val*b)
+        
+        
 
-
+    # Otheer
     def __abs__(self):
         return Tropical(abs(self.val))
 
 
     def __str__(self):
         return str(self.val)
+    
+    def __repr__(self):
+        return str(self.val)
+    
+    def __float__(self):
+        return float(self.val)
+    
+    
+    def sym(self):
+        return Tropical(-self.val)
+    
+    
+    def __truediv__(self,b):
+        return self * b.sym()
+    
+
+    def __floordiv__(self,b):
+        return self * b.sym()
