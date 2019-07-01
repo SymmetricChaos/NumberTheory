e0e2c75dbffe1d4e3ad9b8bcf1e7abfa98b6500e
SymmetricChaos <ajfraebel@gmail.com>
2019-07-01 18:39:46 -0400

    Update PolynomialMultiType.py

--- Polynomials/PolynomialMultiType.py
+++ Polynomials/PolynomialMultiType.py
@@ -36,20 +36,25 @@ class Particle:
     def __init__(self,A):
         assert type(A) == list
         assert all([type(a) == Atom for a in A])
-        self.A = A
-        print(sorted(A))
+        self.A = sorted(A)
         
     def __lt__(self,other):
         assert type(other) == Particle
         x = sum([a.p for a in self.A])
         y = sum([a.p for a in other.A])
         return x < y
+    
+    def __str__(self):
+        out = ""
+        for a in self.A:
+            out += str(a)
+        return out
         
     
 class PolyMult:
     """Polynomial with various indeterminates"""
     
-    def __init__(self):
+    def __init__(self,P,C):
         pass
 
 
@@ -57,4 +62,6 @@ a = Atom("a",3)
 b = Atom("b",1)
 c = Atom("c",2)
 
-Particle([b,c,a])
+print(Particle([b,c,a]))
+
+print(Particle([b,a]))
