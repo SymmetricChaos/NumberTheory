0e8410d2492b944c1e65e9cc89d82f8af3c7205a
SymmetricChaos <ajfraebel@gmail.com>
2019-07-04 16:09:01 -0400

    Error message

--- WaniKani/WKAPI.py
+++ WaniKani/WKAPI.py
@@ -6,15 +6,23 @@ def WKrequest(S,token):
     wkurl = "https://api.wanikani.com/v2/"
     resp = requests.get(wkurl+S,headers=token)
     J = json.loads(resp.content.decode('utf-8'))
+    if "error" in J:
+        raise Exception(f"Error {J['code']} {J['error']}\nMade request {wkurl+S}")
     return J
 
-J = WKrequest("level_progressions",wktoken)
 
-for i in J:
-    print(i)
+
+J = WKrequest("reviews",wktoken)
 
 for i in J["data"]:
     print(i)
     print()
+
+J = WKrequest("study_materials/2559",wktoken)
+
+for i in J["data"]:
+    print(i)
+    print()
+
     
     
