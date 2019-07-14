c41cc1c77a379588cd53f0d6ef2c475e83942427
SymmetricChaos <ajfraebel@gmail.com>
2019-07-14 12:24:32 -0400

    Update PolynomialMultiType.py

--- Polynomials/PolynomialMultiType.py
+++ Polynomials/PolynomialMultiType.py
@@ -17,33 +17,41 @@ class Atom:
         if self.s == other.s and self.p == other.p:
             return True
         return False
-    
+
+
     def __lt__(self,other):
         assert type(other) == Atom
         return self.s < other.s
-    
+
+
     def __str__(self):
         if self.p == 1:
             return f"{self.s}"
         return f"{self.s}^{self.p}"
-    
+
+
     def __repr__(self):
         if self.p == 1:
             return f"{self.s}"
         return f"{self.s}^{self.p}"
-    
+
+
     def __add__(self,other):
         return Particle([self]) + other
-    
+
+
     def __radd__(self,other):
         return Particle([self]) + other
-    
+
+
     def __sub__(self,other):
         return Particle([self]) - other
-    
+
+
     def __rsub__(self,other):
         return Particle([self]) - other
-    
+
+
     def __mul__(self,other):
         if type(other) == Atom:
             # If they are Atom with the same indeterminate sum their powers
@@ -55,10 +63,12 @@ class Atom:
             return other*self
         
         return Particle([self],other)
-                
+
+
     def __rmul__(self,other):
         return self*other
-    
+
+
     def __pow__(self,other):
         assert type(other) == int
         assert other >= 0
@@ -85,9 +95,20 @@ class Particle:
         y = sum([a.p for a in other.A])
         return x > y
     
+    
+    def __eq__(self,other):
+        if len(self.A) == len(other.A):
+            for i,j in zip(self.A,other.A):
+                if i != j:
+                    return False
+            return True
+        return False
+
+
     def __abs__(self):
         return Particle(self.A,abs(self.coef))
-    
+
+
     def __str__(self):
         if self.A == []:
             return str(self.coef)
@@ -95,13 +116,15 @@ class Particle:
         for a in self.A:
             out += str(a)
         return out
-    
+
+
     def __repr__(self):
         out = str(self.coef) if self.coef != 1 else ""
         for a in self.A:
             out += str(a)
         return out
-    
+
+
     def __mul__(self,other):
         # When multiplied by another particle merge their atoms then sort
         if type(other) == Particle:
@@ -132,9 +155,11 @@ class Particle:
         
         # When multiplied by an number just change the coefficient
         return Particle(self.A,self.coef*other)
-    
+
+
     def __rmul__(self,other):
         return self*other
+
 
     def eval(self,V):
         """Evaluate all indeterminates of the Particle"""
@@ -143,7 +168,8 @@ class Particle:
         for a in self.A:
             out *= V[a.s]**a.p
         return out*self.coef
-    
+
+
     def __add__(self,other):
         if type(other) == Atom:
             return MVPoly([self,Particle([other])])
@@ -153,13 +179,15 @@ class Particle:
             else:
                 return MVPoly([self,other])
         return MVPoly([self,Particle([],other)])
-        
+
+
     def __sub__(self,other):
         if type(other) == Atom:
             return MVPoly([self,Particle([other*-1])])
         if type(other) == Particle:
             return MVPoly([self,other*-1])
         return MVPoly([self,Particle([],-other)])
+
 
     def particle_id(self):
         out = ""
@@ -181,13 +209,24 @@ class MVPoly:
     
     def __init__(self,terms):
         self.terms = poly_merge(sorted(terms))
-    
+
+
     def __str__(self):
         out = str(self.terms[0])
         for term in self.terms[1:]:
             sgn = "-" if term.coef < 0 else "+"
             out +=  " " + sgn + " " + str(abs(term))
         return out
+    
+    
+    def __eq__(self,other):
+        if len(self.terms) == len(other.terms):
+            for i,j in zip(self.terms,other.terms):
+                if i != j:
+                    return False
+            return True
+        return False
+    
     
     def eval(self,V):
         """Evaluate each particle of the polynomial"""
@@ -196,12 +235,7 @@ class MVPoly:
         for p in self.terms:
             out += p.eval(V)
         return out
-    
-#    def merge(self):
-#        """Merge together particles of the same kind"""
-#        ctr = 0
-#        while True:
-#            for 
+
 
     def __mul__(self,other):
         """Multiply together two polynomials"""
@@ -220,7 +254,13 @@ class MVPoly:
     
     def __add__(self,other):
         """Add together two polynomials"""
-        return MVPoly(self.terms+other.terms)
+        if type(other) == MVPoly:
+            return MVPoly(self.terms+other.terms)
+        if type(other) == Particle:
+            return MVPoly(self.terms+[other])
+        if type(other) == Atom:
+            return self + Particle([other])
+        return self + Particle([],other)
     
 
 a = Atom("a")
@@ -250,4 +290,5 @@ def poly_merge(L):
     return terms
 
 print(pp)
-print(pp*(a-1))
+print(pp+(a-1))
+print(pp+1)
