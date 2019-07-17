8c615b14fe9f440dbb1c4aa0bb7d2faa2d8d51e2
SymmetricChaos <ajfraebel@gmail.com>
2019-07-17 19:41:26 -0400

    Create PolynomialMultiExamples.py

---
+++ Polynomials/PolynomialMultiExamples.py
@@ -0,0 +1,13 @@
+from PolynomialMultiType import Atom
+    
+a = Atom("a")
+b = Atom("b")
+c = Atom("c")
+
+
+print(f"a = {a}")
+print(f"a*b = {a*b}")
+print(f"a*a = {a*a}")
+print(f"a**3 = {a**3}")
+print(f"1-a = {1-a}")
+print(f"a*a+a*b = {a*a+a*b}")
