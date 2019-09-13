9db6727c33e6a558db83a7c566685d19ccc5a117
SymmetricChaos <ajfraebel@gmail.com>
2019-09-13 07:44:17 -0400

    Update QuadraticIntegerRing.py

--- Numerals/QuadraticIntegerRing.py
+++ Numerals/QuadraticIntegerRing.py
@@ -1,21 +1,35 @@
 class QuadraticInt:
-    def __init__(self,q,m=1):
+    def __init__(self,q,m=1,n=0):
         assert type(q) == int
         assert type(m) == int
+        assert type(n) == int
         self.q = q
         self.m = m
+        self.n = n
 
         
     def __str__(self):
-        if self.m == 0:
+        # Zero
+        if self.m == 0 and self.n == 0:
             return "0"
-        elif self.m == 1:
+        # One
+        elif self.m == 0 and self.n == 1:
+            return f"1"
+        # Just the quadratic
+        elif self.m == 1 and self.n == 0:
             return f"√{self.q}"
-        else:
+        # Integer part
+        elif self.m == 0 and self.n != 1:
+            return f"{self.n}"
+        # Quadratuc part
+        elif self.m != 1 and self.n == 0:
             return f"{self.m}√{self.q}"
+        # Full number
+        else:
+            return f"{self.n} + {self.m}√{self.q}"
 
     
-    def same_ring(self,other):
+    def from_same_ring(self,other):
         assert type(other) == QuadraticInt
         assert other.q == self.q
 
