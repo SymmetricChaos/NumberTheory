65bbeac31e9ecaca768284b6aaaa56bbd17c781f
SymmetricChaos <ajfraebel@gmail.com>
2019-09-12 21:05:49 -0400

    Update QuadraticIntegerRing.py

--- Numerals/QuadraticIntegerRing.py
+++ Numerals/QuadraticIntegerRing.py
@@ -1,7 +1,10 @@
 class QuadraticInt:
     def __init__(self,q,m=1):
+        assert type(q) == int
+        assert type(m) == int
         self.q = q
         self.m = m
+
         
     def __str__(self):
         if self.m == 0:
@@ -10,4 +13,14 @@ class QuadraticInt:
             return f"√{self.q}"
         else:
             return f"{self.m}√{self.q}"
-        
+
+    
+    def same_ring(self,other):
+        assert type(other) == QuadraticInt
+        assert other.q == self.q
+
+#    def __add__
+#    def __sub__
+#    def __mul__
+#    Its a ring so no division needed
+#    Maybe factoring
