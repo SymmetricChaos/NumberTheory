068fe465d865808aff22bfffe68ef8a70685b415
SymmetricChaos <ajfraebel@gmail.com>
2019-09-17 07:55:34 -0400

    better example

--- Polynomials/PolynomialIntegerTypePlotExample.py
+++ Polynomials/PolynomialIntegerTypePlotExample.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Polynomials.PolynomialIntegerType import IntPolynomial
 
-P = IntPolynomial([3,2,3,0,-196,50,-18,-6,1,0,1])
+P = IntPolynomial([0,2,3,0,-116,20,-18,-6,1,0,1])
 
-x = list(np.linspace(-3,3,101))
+x = list(np.linspace(-2.5,2.5,101))
 y = P.evaluate(x)
 plt.plot(x,y)
+print(P.coef)
+print(str(P))
 plt.title(P.pretty_name())
-print(P.pretty_name())
