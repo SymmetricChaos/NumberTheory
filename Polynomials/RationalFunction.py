d588c7288a1ce9702a555a7f0348d15ad80857f1
SymmetricChaos <ajfraebel@gmail.com>
2019-09-20 17:35:33 -0400

    Create RationalFunction.py

---
+++ Polynomials/RationalFunction.py
@@ -0,0 +1,11 @@
+from Polynomials.PolynomialRationalType import QPoly
+
+## TODO: rational fractions will be ratios of QPoly
+## TODO: rational expressions will be sums of rational fraction
+
+def RationalFrac:
+    
+    def __init__(self):
+        
+        
+    def evaluate():
