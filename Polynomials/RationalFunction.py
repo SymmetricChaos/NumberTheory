def325036900b7e86ac30451832603d18e319900
SymmetricChaos <ajfraebel@gmail.com>
2019-09-20 22:08:33 -0400

    Update RationalFunction.py

--- Polynomials/RationalFunction.py
+++ Polynomials/RationalFunction.py
@@ -5,7 +5,26 @@ from Polynomials.PolynomialRationalType import QPoly
 
 def RationalFrac:
     
-    def __init__(self):
-        
-        
+    def __init__(self,n,d):
+        assert type(n) == QPoly
+        assert type(d) == QPoly
+        self.n = n
+        self.d = d
+      
+    
+    def __str__(self):
+        return f"{str(self.n)} / {str(self.d)}"
+
+
     def evaluate():
+        pass
+    
+    
+    # Will have to return a new object because type could change
+    def simplify(self):
+        pass
+    
+    
+    #LaTeX formatting
+    def pretty_name(self):
+        return 
