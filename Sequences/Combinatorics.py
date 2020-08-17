b103c4ae1bcfc3430653ef08baa2802d40527bab
SymmetricChaos <ajfraebel@gmail.com>
2020-08-17 07:46:48 -0400

    combinatorics tests

--- Sequences/Combinatorics.py
+++ Sequences/Combinatorics.py
@@ -45,6 +45,36 @@ def pascal():
             k += 1
 
 
+def gould():
+    """Gould's Sequence: Number of odd values on the nth row of Pascal's Triangle"""
+    
+    P = pascal()
+    
+    for n in naturals(1):
+        val = 0
+        
+        for i in range(n):
+            if next(P) % 2 == 1:
+                val += 1
+        
+        yield val
+
+
+def eulerian():
+    """Eulerian Triangle: Triangle with number of permutations of a set with n elements where there are m increases"""
+    
+    for m in naturals(1):
+        for n in range(m):
+            S = 0
+            sign = -1
+            
+            for k in range(n+1):
+                sign *= -1
+                S += sign*choose(m+1,k)*(n+1-k)**m
+            
+            yield S
+
+
 def partition():
     """Partition Number: Number of ways to add naturals greater than 0 to get n"""
     
@@ -86,39 +116,49 @@ def bell():
         R0, R1 = R1, R2
 
 
-def eulerian():
-    """Eulerian Numbers: Triangle with number of permutations of a set with n elements where there are m increases"""
-    
-    for m in naturals(1):
-        for n in range(m):
-            S = 0
-            sign = -1
-            
-            for k in range(n+1):
-                sign *= -1
-                S += sign*choose(m+1,k)*(n+1-k)**m
-            
-            yield S
-
-
-def gould():
-    """Gould's Sequence: Number of odd values on the nth row of Pascal's Triangle"""
-    
-    P = pascal()
-    
-    for n in naturals(1):
-        val = 0
-        
-        for i in range(n):
-            if next(P) % 2 == 1:
-                val += 1
-        
-        yield val
-
-
 # Generalized cake numbers? Uses binomial coefficients
 def cake():
     """Cake Numbers: Maximum number of pieces produced when cutting a cube with exactly n planes"""
     
     for n in naturals():
         yield (n*n*n+5*n+6)//6
+
+
+
+
+
+if __name__ == '__main__':
+    from Sequences.SequenceManipulation import simple_test
+    
+    print("Derangement Numbers")
+    simple_test(derangements(),9,
+                "1, 0, 1, 2, 9, 44, 265, 1854, 14833")
+    
+    print("\nCatalan Numbers")
+    simple_test(catalan(),9,
+                "1, 1, 2, 5, 14, 42, 132, 429, 1430")
+    
+    print("\nPascal's Triangle by Rows")
+    simple_test(pascal(),10,
+                "1, 1, 1, 1, 2, 1, 1, 3, 3, 1")
+    
+    print("\nGould's Sequence")
+    simple_test(gould(),10,
+                "1, 2, 2, 4, 2, 4, 4, 8, 2, 4")
+    
+    print("\nEulerian Triangle by Rows")
+    simple_test(eulerian(),10,
+                "1, 1, 1, 1, 4, 1, 1, 11, 11, 1")
+    
+    print("\nPartition Numbers")
+    simple_test(partition(),10,
+                "1, 1, 2, 3, 5, 7, 11, 15, 22, 30")
+    
+    print("\nBell Numbers")
+    simple_test(bell(),9,
+                "1, 1, 2, 5, 15, 52, 203, 877, 4140")
+    
+    print("\nCake Numbers")
+    simple_test(cake(),9,
+                "1, 2, 4, 8, 15, 26, 42, 64, 93")
+    
