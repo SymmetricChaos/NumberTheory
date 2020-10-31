4cd3b1b58347b7f423e92410a4801df6c2f27a14
SymmetricChaos <ajfraebel@gmail.com>
2020-10-30 20:26:08 -0400

    much more efficient lucas pseudo prime sequences

--- Sequences/Pseudoprimes.py
+++ Sequences/Pseudoprimes.py
@@ -1,4 +1,5 @@
 from Sequences.Primes import composites
+from Sequences.MathUtils import factor_out_twos
 
 from sympy import jacobi_symbol
 from math import gcd
@@ -40,16 +41,14 @@ def strong_pseudoprimes(a):
             d = c-1
             r = 0
             
-            while d % 2 == 0:
-                d //= 2
-                r += 1
+            d,r = factor_out_twos(d)
             
             if (a**d)%c == 1:
                 yield c
                 continue
             
             for e in range(0,r):
-                if (a**(d*2**e))%c == c-1:
+                if pow(a,(d*2**e),c) == c-1:
                     yield c
                     continue
 
@@ -60,15 +59,19 @@ def lucas_pseudoprimes(P,Q):
     """
     
     D = P*P - 4*Q
+    lucas_pos = 0 #rather than recompute we will step the sequence as needed
+    a,b = 0,1
+    
     for c in composites():
         if c % 2 == 1:
             if gcd(c,Q) == 1:
                 delta = c-jacobi_symbol(D,c)
                 
-                a,b = 0,1
-                
-                for i in range(delta):
-                    a, b = b, P*b-Q*a
+                if delta > lucas_pos:
+                    for i in range(delta-lucas_pos):
+                        a, b = b, P*b-Q*a
+                    
+                    lucas_pos += delta-lucas_pos
                 
                 if a % c == 0:
                     yield c
@@ -79,15 +82,19 @@ def fibonacci_pseudoprimes():
     Fibonacci Pseudoprimes: Special case of Lucas Pseudoprimes, also excluding multiples of 5
     OEIS A081264
     """
+
+    lucas_pos = 0 
+    a,b = 0,1
     
     for c in composites():
         if c % 2 == 1 and c % 5 != 0:
             delta = c-jacobi_symbol(5,c)
             
-            a,b = 0,1
-            
-            for i in range(delta):
-                a, b = b, b+a
+            if delta > lucas_pos:
+                for i in range(delta-lucas_pos):
+                    a, b = b, b+a
+                
+                lucas_pos += delta-lucas_pos
             
             if a % c == 0:
                 yield c
@@ -112,6 +119,44 @@ def pell_pseudoprimes():
                 yield c
 
 
+# def strong_lucas_pseudoprimes(P,Q):
+#     """
+#     Lucas Pseudoprimes: Composite number passing the Lucas primality test for P and Q
+#     """
+    
+#     def lucas_U_test(n,d,P,Q):
+#         a,b = 0,1
+#         for i in range(d):
+#             a, b = b, (P*b-Q*a)%c
+#         if a == 0:
+#             return True
+#         return False
+    
+#     def lucas_V_test(d,s,P,Q):
+#         a, b = 2, P
+#         for i in range(s):
+#             for k in range(d):
+#                 a, b = b, P*b-Q*a
+#             if a%c == 0:
+#                 return True
+#         return False
+    
+#     D = P*P - 4*Q
+#     for c in composites():
+#         if c % 2 == 1:
+#             if gcd(c,D) == 1:
+#                 delta = c-jacobi_symbol(D,c)
+#                 s,d = factor_out_twos(delta)
+                
+#                 # Check with a Lucas U-Sequence
+#                 if lucas_U_test(c,d,P,Q):
+#                     yield c
+#                     continue
+                
+#                 # Check with a Lucas V-Sequence
+#                 if lucas_V_test(d,s,P,Q):
+#                     yield c
+
 
 
 
@@ -130,9 +175,9 @@ if __name__ == '__main__':
     simple_test(strong_pseudoprimes(3),9,
                 "121, 703, 1891, 3281, 8401, 8911, 10585, 12403, 16531")
     
-    # print("\n(3,-1)-Lucas Pseudoprimes")
-    # simple_test(lucas_pseudoprimes(3,-1),9,
-    #             "121, 703, 1891, 3281, 8401, 8911, 10585, 12403, 16531")
+    print("\n(3,-1)-Lucas Pseudoprimes")
+    simple_test(lucas_pseudoprimes(3,-1),9,
+                "119, 169, 649, 1189, 1763, 2197, 3599, 4187, 5559")
     
     print("\nFibonacci Pseudoprimess")
     simple_test(fibonacci_pseudoprimes(),9,
@@ -140,5 +185,9 @@ if __name__ == '__main__':
     
     # print("\nPell Pseudoprimess")
     # simple_test(pell_pseudoprimes(),9,
+    #             "323, 377, 1891, 3827, 4181, 5777, 6601, 6721, 8149")
+    
+    # print("\nStrong Lucas Pseudoprimess")
+    # simple_test(strong_lucas_pseudoprimes(1,-1),2,
     #             "323, 377, 1891, 3827, 4181, 5777, 6601, 6721, 8149")
     
