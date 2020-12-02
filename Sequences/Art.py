dfce951f817db801b58037a5bcfe9877fa1dc00b
SymmetricChaos <ajfraebel@gmail.com>
2020-12-02 17:23:16 -0500

    Update Art.py

--- Sequences/Art.py
+++ Sequences/Art.py
@@ -2,22 +2,22 @@ from Sequences import Drawing as draw
 import numpy as np
 
 
-def centered_triangular(n):
+def centered_figurate(k,n):
     
     draw.make_blank_canvas([15,15],facecolor="lightgray")
-    draw.make_blank_plot(1,1,1,[-4,4],[-3.5,4.5])
+    draw.make_blank_plot(1,1,1,[-4,4],[-4,4])
     
     draw.circle_xy(0,0,.1)
     
-    th = np.linspace(0,2*np.pi,4)
+    th = np.linspace(0,2*np.pi,k+1)
     
     for i in range(1,n+1):
-        x = np.sin(th[:3])*(4*i/n)
-        y = np.cos(th[:3])*(4*i/n)
+        x = np.sin(th[:k])*(3.5*i/n)
+        y = np.cos(th[:k])*(3.5*i/n)
         
-        for p in range(3):
-            x_space = np.linspace(x[p%3],x[(p+1)%3],i+1)
-            y_space = np.linspace(y[p%3],y[(p+1)%3],i+1)
+        for p in range(k):
+            x_space = np.linspace(x[p%k],x[(p+1)%k],i+1)
+            y_space = np.linspace(y[p%k],y[(p+1)%k],i+1)
             draw.circles_xy(x_space,y_space,[.1]*(i+1))
 
-centered_triangular(7)
+centered_figurate(5,5)
